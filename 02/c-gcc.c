adef48343f4729263288e398dd5daafb47aa4176
moratorium08 <moratorium08@users.noreply.github.com>
2017-03-27 12:38:36 +0900

    Update c-gcc.c (105 bytes)

--- 02/c-gcc.c
+++ 02/c-gcc.c
@@ -1,1 +1,1 @@
-char c,s[10],i=100;main(){while(i--){read(0,&c,1);s[c-48]++;}for(;i<10;i++){while(s[i]--)printf("%d",i);}}
+char c,s[10],i=100;main(){while(i--){read(0,&c,1);s[c-48]++;}while(10-i++){while(s[i]--)printf("%d",i);}}
