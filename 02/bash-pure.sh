ef29b2969ace0783ae368f361f6688723adc1c09
hakatashi <hakatashi@users.noreply.github.com>
2017-03-27 16:55:00 +0900

    Update bash-pure.sh (295 bytes)

--- 02/bash-pure.sh
+++ 02/bash-pure.sh
@@ -3,23 +3,23 @@ while read -n 1 c; do
 a+=($c)
 done
 q(){
-   local k i s=() l=()
-   p=()
-   (($#==0)) && return 0
-   k=$1
-   shift
-   for i; do
-      if [[ $i < $k ]]; then
-         s+=( "$i" )
-      else
-         l+=( "$i" )
-      fi
-   done
-   q "${s[@]}"
-   s=( "${p[@]}" )
-   q "${l[@]}"
-   l=( "${p[@]}" )
-   p=( "${s[@]}" "$k" "${l[@]}" )
+local k i s=() l=()
+p=()
+(($#==0))&&return 0
+k=$1
+shift
+for i; do
+if [[ $i < $k ]]; then
+s+=("$i")
+else
+l+=("$i")
+fi
+done
+q "${s[@]}"
+s=("${p[@]}")
+q "${l[@]}"
+l=("${p[@]}")
+p=("${s[@]}" "$k" "${l[@]}")
 }
 q "${a[@]}"
 b=$(IFS=;echo "${p[*]}")
