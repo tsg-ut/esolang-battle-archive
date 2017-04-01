032165246108da7f2276181c8f7d93b1b33e9a48
hakatashi <hakatashi@users.noreply.github.com>
2017-04-02 01:01:32 +0900

    Update bash-pure.sh (198 bytes)

--- 02/bash-pure.sh
+++ 02/bash-pure.sh
@@ -1,4 +1,4 @@
 a=()
 while read -n 1 c;do a+=($c);done
-for((i=0;i<100;i++));do for((j=0;j<99;j++));do k=$((j+1));if [[ ${a[j]} > ${a[k]} ]];then t=${a[k]};a[k]=${a[j]};a[j]=$t;fi;done;done
+for((i=0;i<100;i++));do for((j=0;j<99;j++));do k=$((j+1));if [[ ${a[j]}>${a[k]} ]];then t=${a[k]};a[k]=${a[j]};a[j]=$t;fi;done;done
 echo $(IFS=;echo "${a[*]}")
