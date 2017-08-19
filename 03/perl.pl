c7b3a597354f072b11f9f7d0bd12dbffbbed2b5a
moratorium08 <moratorium08@users.noreply.github.com>
2017-08-19 12:17:10 +0900

    Update perl.pl (121 bytes)

--- 03/perl.pl
+++ 03/perl.pl
@@ -1,6 +1,1 @@
-use POSIX qw(floor);
-while(defined($l=<STDIN>)){
-    $x=1+8*unpack("C",pack("B8",$l));
-    $y=int((sqrt($x)));
-    print int($y*$y/$x);
-}
+use POSIX qw(floor);while(defined($l=<STDIN>)){$x=1+8*unpack("C",pack("B8",$l));$y=int((sqrt($x)));print int($y*$y/$x);}
