0606851feb6c78cc839753ac8bfd2431bd09f0c3
Liesegang <Liesegang@users.noreply.github.com>
2017-08-19 12:32:11 +0900

    Update python3.py (108 bytes)

--- 03/python3.py
+++ 03/python3.py
@@ -2,7 +2,4 @@ s=[0]
 for i in range(1,23):
 	s.append(s[-1]+i)
 for i in range(0,50):
-	if int(input(),2) in s:
-		print(1,end="")
-	else:
-		print(0,end="")
+	print(int(int(input(),2) in s),end="")
