772b2bbff040a832209301a90bcc2528cb626357
progrunner17 <progrunner17@users.noreply.github.com>
2017-08-19 14:31:29 +0900

    Update c-gcc.c (128 bytes)

--- 03/c-gcc.c
+++ 03/c-gcc.c
@@ -1,1 +1,1 @@
-int i,a,b;main(){for(;a<50;a++){for(b=i=0;b<8;b++){i+=getchar()&1;i*=2;}getchar();i/=2;for(b=0;i>0;b++)i-=b;putchar(i<0?48:49);}}
+int i,a,b;main(){for(;a<50;a++){for(b=i=0;b<8;b++){i+=getchar()&1;i*=2;}getchar();i/=2;for(b=0;i>0;)i-=b++;putchar(i<0?48:49);}}
