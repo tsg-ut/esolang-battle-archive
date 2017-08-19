5f908bb976f2064636d62c8fb4d6fb5fe6668ddc
cookie-s <cookie-s@users.noreply.github.com>
2017-08-19 17:38:55 +0900

    Update rdmd.d (134 bytes)

--- 03/rdmd.d
+++ 03/rdmd.d
@@ -1,1 +1,1 @@
-import std.stdio;import std.conv;import std.math;void main(){for(int i=50;i--;){writeln(sqrt(to!int(readln()[0..8],2)*8+1.)%2==1?1:0);}}
+import std.stdio;import std.conv;import std.math;void main(){for(int i=50;i--;){write(sqrt(to!int(readln()[0..8],2)*8+1.)%2==1?1:0);}}
