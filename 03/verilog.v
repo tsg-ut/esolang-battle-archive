dfce7e920f13a86563fce277bbccdb1f0b6ce66c
progrunner17 <progrunner17@users.noreply.github.com>
2017-08-19 14:11:22 +0900

    Update verilog.v (290 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -1,2 +1,2 @@
 `define S 32'h8000_0000
-module cat;integer c,i,d;initial begin c=$fgetc(`S);while(!(&c))begin d=0;i=1;while(c==48||c==49)begin d=d*2;d=d+c-48;c=$fgetc(`S);end i=1;while(d>0)begin d=d-i;i=i+1;end if(d==0)begin $write("%c ","1");end else begin $write("%c ","0");end c=$fgetc(`S);end end endmodule
+module cat;integer c,i,d;initial begin c=$fgetc(`S);while(!(&c))begin d=0;i=1;while(c==48||c==49)begin d=d*2;d=d+c-48;c=$fgetc(`S);end i=1;while(d>0)begin d=d-i;i=i+1;end if(d==0)begin $write("%d ",1);end else begin $write("%d ",0);end c=$fgetc(`S);end end endmodule
