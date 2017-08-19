a73b3c527c805504cff49cabc6e88e0c449f68c3
cookie-s <cookie-s@users.noreply.github.com>
2017-08-19 15:54:40 +0900

    Update csharp.cs (138 bytes)

--- 03/csharp.cs
+++ 03/csharp.cs
@@ -1,1 +1,1 @@
-using System;class E{static void Main(){for(int i=51;--i>0;)Console.Write(Math.Sqrt(8*Convert.ToInt32(Console.ReadLine(),2)+1)%2==1?1:0);}}
+using System;class E{static void Main(){for(int i=51;--i>0;)Console.Write(Math.Sqrt(8*Convert.ToByte(Console.ReadLine(),2)+1)%2==1?1:0);}}
