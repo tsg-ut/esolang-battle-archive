05f5342d1512e74e9dec4f11218588e696b3ae9c
satos---jp <satos---jp@users.noreply.github.com>
2017-08-19 16:18:17 +0900

    Update verilog.v (209 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -1,11 +1,4 @@
-module a;integer i,j,x,c;
-	initial begin
-		for (i=0;i<50;i=i+1) begin
-			c = $fscanf(32'h8000_0000,"%b",x);
-			for(j=0;j<=x;j=j+1) begin
-				if (j*(j+1)/2==x) begin
-					c = 0;
-				end
+module a;integer i,j,x,c;initial begin for (i=0;i<50;i=i+1) begin c = $fscanf(32'h8000_0000,"%b",x);for(j=0;j<=x;j=j+1) begin if (j*(j+1)/2==x) begin c = 0;end 
 			end
 			$write("%d",1-c);
 		end
