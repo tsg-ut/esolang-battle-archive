e8350cdda6378ee2add4bd5df2ae702d816943ad
Yosshi999 <Yosshi999@users.noreply.github.com>
2017-08-19 14:56:43 +0900

    Update c-gcc.c (109 bytes)

--- 03/c-gcc.c
+++ 03/c-gcc.c
@@ -1,1 +1,1 @@
-int i,a,b;main(){for(;a<50;a++){for(b=i=0;b<9;b++,i=i*2|getchar()&1);i/=2;for(b=0;i>0;)i-=b++;putchar(i?48:49);}}
+int i,a,b;main(){for(;a<50;a++){i=0;b=9;while(b--)i=i*2|getchar()&1;i/=2;while(i>0)i-=++b;putchar(i?48:49);}}
