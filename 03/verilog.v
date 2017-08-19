7788d2a5544d374e74fac8744ddbf87f363a7c11
progrunner17 <progrunner17@users.noreply.github.com>
2017-08-19 14:01:47 +0900

    Update verilog.v (476 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -3,10 +3,7 @@
 
 module cat;
 	integer c,i,d;
-	initial begin c = $fgetc(`STDIN);
-		while (c != `EOF) begin
-			d = 0;
-			i = 1;
+	initial begin c = $fgetc(`STDIN);while (c != `EOF) begin d = 0;i = 1;
 			while ( c == 48 || c == 49)begin
 				d = d * 2;
 				d = d + c - 48; 
