a3d19334cc7acb061fbfd9bb33f445a3508630e5
kurgm <kurgm@users.noreply.github.com>
2017-08-19 15:55:54 +0900

    Update node.js (93 bytes)

--- 03/node.js
+++ 03/node.js
@@ -1,1 +1,1 @@
-with(require("fs"))readFileSync(0,"utf8").match(/.+/g).map(q=>write(1,+(`0b${q}001`**0.5%2==1)))
+with(require("fs"))readFileSync(0,"utf8").match(/.+/g).map(q=>write(1,+!(`0b${q}001`**.5%1)))
