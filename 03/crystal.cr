fb48654e4b490f8852eb3a2f877912f6cf1c4c9b
progrunner17 <progrunner17@users.noreply.github.com>
2017-08-19 12:16:51 +0900

    Update crystal.cr (162 bytes)

---
+++ 03/crystal.cr
@@ -0,0 +1,21 @@
+while a = gets()
+c = a.to_i
+b = 0
+i = 0
+while i<8
+r = c % 10
+c /=  10
+b+=r*(2**i)
+i+=1
+end
+i = 1
+while b > 0
+b -= i
+i+=1
+end
+if b ==0
+puts(1)
+else
+puts(0)
+end
+end
