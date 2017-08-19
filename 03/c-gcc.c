a8c69f6f49afb35918e10d223b85af7d5ba7bdf5
Yosshi999 <Yosshi999@users.noreply.github.com>
2017-08-19 14:15:47 +0900

    Update c-gcc.c (147 bytes)

--- 03/c-gcc.c
+++ 03/c-gcc.c
@@ -1,1 +1,2 @@
-int m(int x){int lb=0;int ub=x;while(ub-lb > 1){int mb=(ub+lb)/2;if(mb*mb>x)ub=mb;else lb=mb;}return lb;}char s[9];main(){for(int i=0;i<50;i++){scanf("%s", s);int x=0;for(int j=0;j<8;j++){x*=2;x+=s[j]=='1';}int y=1+8*x;int z=y!=1?m(y):1;printf("%d",z*z/y);}}
+#include<stdio.h>
+int i,a,b;main(){for(;a<50;a++){for(b=i=0;b<8;b++){i+=getchar()&1;i*=2;}getchar();i/=2;for(b=0;i>0;b++)i-=b;putchar(i<0?48:49);}}
