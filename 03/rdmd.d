a80590f7f618cf5c8e34ef5640ccde0d5c8ac15a
Liesegang <Liesegang@users.noreply.github.com>
2017-08-19 17:31:16 +0900

    Update rdmd.d (142 bytes)

--- 03/rdmd.d
+++ 03/rdmd.d
@@ -1,19 +1,7 @@
 import std.stdio;
-import std.array;
-import std.math;
 import std.conv;
-
-bool issq(int a) {return cast(int)sqrt(cast(real)a)*cast(int)sqrt(cast(real)a) == a;}
-
-bool tri(int a) {return issq(8*a+1)&&(cast(int)sqrt(8.0*a+1) % 2);}
-void main() {
-string s;
-while ((s = readln()).length > 3) {
-int i = to!int(split(s)[0],2);
-if(tri(i)) {
-writeln("1");
-} else {
-writeln("0");
-}
-}
-}
+import std.math;
+void main(){
+for(int i=50;i--;){
+writeln(sqrt(to!int(readln()[0..8],2)*8+1.)%2==1?1:0);
+}}
