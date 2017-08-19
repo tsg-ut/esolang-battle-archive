2706fef582a49962b072b14a8ffe620d80e7afb2
progrunner17 <progrunner17@users.noreply.github.com>
2017-08-19 16:02:15 +0900

    Update verilog.v (261 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -1,1 +1,1 @@
-module cat;integer c,i,d,s;initial begin s=1<<31; c=$fgetc(s);while(!(&c))begin d=0;while(c!=10)begin d=d*2;d=d+c-48;c=$fgetc(s);end i=1;while(d>0)begin d=d-i;i=i+1;end if(d==0)begin $write("%d ",1);end else begin $write("%d ",0);end c=$fgetc(s);end end endmodule
+module c;integer c,i,d,s;initial begin s=1<<31; c=$fgetc(s);while(!(&c))begin d=0;while(c!=10)begin d=d*2;d=d+c-48;c=$fgetc(s);end i=1;while(d>0)begin d=d-i;i=i+1;end if(d==0)begin $write("%d ",1);end else begin $write("%d ",0);end c=$fgetc(s);end end endmodule
