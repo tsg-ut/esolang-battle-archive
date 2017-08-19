b51bf89f24f5661ec180077a911c5256b1e01962
hakatashi <hakatashi@users.noreply.github.com>
2017-08-19 16:45:53 +0900

    Update sqlite3.sql (609 bytes)

--- 03/sqlite3.sql
+++ 03/sqlite3.sql
@@ -1,8 +1,8 @@
-WITH RECURSIVE m(x,l,r) AS (
-SELECT 1, '' AS l, substr(v, 0) AS r FROM i
+WITH RECURSIVE m(x,l,r,p) AS (
+SELECT 1, '' AS l, substr(v,10) AS r, substr(v,1,8) AS p FROM i
 UNION ALL
-SELECT x + 1, CASE WHEN substr(r,1,8)='00000000' or substr(r,1,8)='00000001' or substr(r,1,8)='00000011' or substr(r,1,8)='00000110' or substr(r,1,8)='00001010' or substr(r,1,8)='00001111' or substr(r,1,8)='00010101' or substr(r,1,8)='00011100' or substr(r,1,8)='00100100' or substr(r,1,8)='00101101' or substr(r,1,8)='00110111' or substr(r,1,8)='01000010' or substr(r,1,8)='01001110' or substr(r,1,8)='01011011' or substr(r,1,8)='01101001' or substr(r,1,8)='01111000' or substr(r,1,8)='10001000' or substr(r,1,8)='10011001' or substr(r,1,8)='10101011' or substr(r,1,8)='10111110' or substr(r,1,8)='11010010' or substr(r,1,8)='11100111' or substr(r,1,8)='11111101' THEN 1 ELSE 0 END AS l, substr(r, 10) AS r
+SELECT x + 1, CASE WHEN p='00000000' or p='00000001' or p='00000011' or p='00000110' or p='00001010' or p='00001111' or p='00010101' or p='00011100' or p='00100100' or p='00101101' or p='00110111' or p='01000010' or p='01001110' or p='01011011' or p='01101001' or p='01111000' or p='10001000' or p='10011001' or p='10101011' or p='10111110' or p='11010010' or p='11100111' or p='11111101' THEN 1 ELSE 0 END AS l, substr(r, 10) AS r, substr(r,1,8) AS p
 FROM m
-WHERE r != ''
+WHERE p != ''
 )
 SELECT l FROM m WHERE l != ''
