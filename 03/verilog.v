0fb7271e4f996f274e6284b84ced6d6984cfe612
cookie-s <cookie-s@users.noreply.github.com>
2017-08-19 18:15:15 +0900

    Update verilog.v (147 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -1,1 +1,1 @@
-module a;integer i,j,x,c;initial repeat(50) begin c=$fscanf(2147483648,"%b",x);for(j=0;j<=x;j=j+1)c=c+(j*(j+1)/2==x);$write("%d",c-1);end endmodule
+module a;integer i,j,x,c;initial repeat(50)begin c=$fscanf(2147483648,"%b",x);for(j=0;j<=x;j=j+1)c=c+(j*(j+1)/2==x);$write("%d",c-1);end endmodule
