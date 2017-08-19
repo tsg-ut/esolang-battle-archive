f10f161e647c5ff9e707a70831ac1d266585edf2
hakatashi <hakatashi@users.noreply.github.com>
2017-08-19 12:59:42 +0900

    Update js-rhino.js (264 bytes)

--- 03/js-rhino.js
+++ 03/js-rhino.js
@@ -1,7 +1,1 @@
-importPackage(java.io);
-importPackage(java.lang);
-const stdin = new BufferedReader(new InputStreamReader(System.in));
-for (i=0;i<50;i++) {
-let aLine = stdin.readLine();
-print(+![0,1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,136,153,171,190,210,231,253].every(function(i){return i-parseInt(aLine,2)}));
-}
+importPackage(java.io);importPackage(java.lang);s=new BufferedReader(new InputStreamReader(System.in));for(i=0;i<50;i++){l=s.readLine();print(+![0,1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,136,153,171,190,210,231,253].every(function(i){return i-parseInt(l,2)}))}
