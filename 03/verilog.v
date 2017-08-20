5b187a6fde24935cda3e772886ab6f24604aa13f
progrunner17 <progrunner17@users.noreply.github.com>
2017-08-20 11:51:57 +0900

    Update verilog.v (122 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -1,1 +1,1 @@
-module c;integer c,d;initial begin repeat(50)begin for(c=$fscanf(1<<31,"%b",d);d>0;c=c+1)d=d-c;$write(~|d);end end endmodule
+module c;integer c,d;initial begin while($fscanf(1<<31,"%b",d))begin for(c=1;d>0;c=c+1)d=d-c;$write(~|d);end end endmodule
