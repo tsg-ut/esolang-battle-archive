becca9f5a4aecf53c14e9cb706f918d55ddb4719
kurgm <kurgm@users.noreply.github.com>
2017-08-20 12:56:21 +0900

    Update csharp.cs (137 bytes)

--- 03/csharp.cs
+++ 03/csharp.cs
@@ -1,1 +1,1 @@
-using System;class E{static void Main(){for(int i=51;--i>0;)Console.Write(Math.Sqrt(8*Convert.ToByte(Console.ReadLine(),2)+1)%2==1?1:0);}}
+using System;class _{static void Main(){for(int i=0;i++<50;Console.Write(Math.Sqrt(1+8*Convert.ToByte(Console.ReadLine(),2))%1>0?0:1));}}
