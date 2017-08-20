d9f378378e5a4ab569a88906b6c93ff9cae7c42a
xuzijian629_key <xuzijian629_key@twitter.com>
2017-08-20 13:29:35 +0900

    Update verilog.v (110 bytes)

--- 03/verilog.v
+++ 03/verilog.v
@@ -1,4 +1,1 @@
-module a;real x,y;initial repeat(50)begin
-y=$fscanf(1<<31,"%b",x);
-$write("%d",(8*x+1)**0.5%2==1);
-end endmodule
+module a;real x,y;initial repeat(50)begin y=$fscanf(1<<31,"%b",x);$write("%d",(8*x+1)**0.5%2==1);end endmodule
