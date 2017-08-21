4eb6cb23c79ef6da53757823aa9087844284d1f9
lip_of_cygnus <lip_of_cygnus@twitter.com>
2017-08-21 13:31:35 +0900

    Update js-rhino.js (126 bytes)

--- 03/js-rhino.js
+++ 03/js-rhino.js
@@ -1,5 +1,1 @@
-cin=new java.util.Scanner(java.lang.System.in);
-for(;cin.hasNext();){
-   l= cin.nextLine();
-    print(+!((Math.sqrt(8*parseInt(l,2)+1)-1)%2)); 
-}
+c=new java.util.Scanner(java.lang.System.in);for(;c.hasNext();){l=c.nextLine();print(+!((Math.sqrt(8*parseInt(l,2)+1)-1)%2));}
