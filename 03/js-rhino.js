63d5137bf187ecf14e75593e2fd3639e0e5f79ca
hakatashi <hakatashi@users.noreply.github.com>
2017-08-21 14:23:08 +0900

    Update js-rhino.js (115 bytes)

--- 03/js-rhino.js
+++ 03/js-rhino.js
@@ -1,1 +1,1 @@
-c=new java.util.Scanner(java.lang.System.in);for(;c.hasNext();){print(+!(Math.sqrt(8*parseInt(c.nextLine(),2)+1)%1));}
+c=new java.util.Scanner(java.lang.System.in);for(;c.hasNext();)print(+!(Math.sqrt(8*parseInt(c.nextLine(),2)+1)%1))
