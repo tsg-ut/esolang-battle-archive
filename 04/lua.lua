97839e18cf29363a2270936611066fc31e4aa0bb
kurgm <kurgm@users.noreply.github.com>
2018-04-30 14:47:51 +0900

    Update lua.lua (193 bytes)

--- 04/lua.lua
+++ 04/lua.lua
@@ -2,10 +2,10 @@ X={0,0}
 X[0]=0
 i=0
 c=1
-while 1 do
+while c do
 c=io.read("*n")
-if not(c) then break end
+if c then
 X[i]=c-X[i%3]
 i=i+1
-end
+end end
 print((X[3]*X[7]*X[11]+X[4]*X[8]*X[9]+X[5]*X[6]*X[10]-X[3]*X[8]*X[10]-X[4]*X[6]*X[11]-X[5]*X[7]*X[9])//6)
