3ff2e516bd5b80f6def8050ce8982fd7fe0b185e
kurgm <kurgm@users.noreply.github.com>
2018-04-30 10:43:06 +0900

    Update c-gcc.c (182 bytes)

--- 04/c-gcc.c
+++ 04/c-gcc.c
@@ -1,2 +1,2 @@
-#define Z(a,b,c) int a,b,c;scanf("%d%d%d",&a,&b,&c);a-=j;b-=k;c-=l;
-j,k,l;main(){scanf("%d%d%d",&j,&k,&l);Z(a,b,c)Z(d,e,f)Z(g,h,i)printf("%d",(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6);}
+#define Z(a,b,c) int a,b,c;scanf(A,&a,&b,&c);a-=j;b-=k;c-=l;
+*A="%d%d%d";j,k,l;main(){scanf(A,&j,&k,&l);Z(a,b,c)Z(d,e,f)Z(g,h,i)printf("%d",(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6);}
