f2902658255b670b20175c960ec7b2e8cb5e65aa
kurgm <kurgm@users.noreply.github.com>
2018-04-30 12:40:14 +0900

    Update bash-busybox.sh (136 bytes)

--- 04/bash-busybox.sh
+++ 04/bash-busybox.sh
@@ -2,7 +2,4 @@ read j k l
 read a b c
 read d e f
 read g h i
-a=$[a-j];b=$[b-k];c=$[c-l]
-d=$[d-j];e=$[e-k];f=$[f-l]
-g=$[g-j];h=$[h-k];i=$[i-l]
-echo $[(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6]
+echo $[a-=j,b-=k,c-=l,d-=j,e-=k,f-=l,g-=j,h-=k,i-=l,(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6]
