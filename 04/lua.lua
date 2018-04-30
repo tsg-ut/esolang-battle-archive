8f68b3fa43a28c19049fea500e5988e244d4cd0b
akouryy <akouryy@users.noreply.github.com>
2018-04-30 14:59:26 +0900

    Update lua.lua (170 bytes)

--- 04/lua.lua
+++ 04/lua.lua
@@ -1,4 +1,4 @@
-a={}for k=1,12 do a[#a+1]=io.read("*n")end
+a={}for k=1,12 do a[k]=io.read("*n")end
 for z=1,9 do a[z]=a[z]-a[10+~-z%3]end
 s=0
 for z=1,3 do q=-~z%3;s=s+a[z]*(a[7+z%3]*a[4+q]-a[4+z%3]*a[7+q])end
