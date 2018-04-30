8301eb208efd9fccd1a7883259d4c73ca4ff6528
kurgm <kurgm@users.noreply.github.com>
2018-04-30 18:59:55 +0900

    Update python3.py (133 bytes)

--- 04/python3.py
+++ 04/python3.py
@@ -1,3 +1,3 @@
-s=[*map(int,open(0).read().split())]
-a,b,c,d,e,f,g,h,i=[s[i+3]-s[i%3]for i in range(9)]
-print((a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)//6)
+s=open(0).read().split()
+a,b,c,d,e,f,g,h,i=[int(s[i+3])-int(s[i%3])for i in range(9)]
+print((a*(e*i-h*f)+b*(f*g-i*d)+c*(d*h-g*e))//6)
