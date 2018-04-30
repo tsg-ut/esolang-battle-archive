4f8f6e52253ac86dc06a97e46b91769b562c9428
kurgm <kurgm@users.noreply.github.com>
2018-04-30 15:06:19 +0900

    Update lua.lua (156 bytes)

--- 04/lua.lua
+++ 04/lua.lua
@@ -1,3 +1,2 @@
-X={}for i=-3,8 do X[i]=0
-X[i]=io.read("*n")-X[i%3-3]end
+X={}for i=-3,8 do X[i]=io.read("*n")-(X[i%3-3]or 0)end
 print((X[0]*X[4]*X[8]+X[1]*X[5]*X[6]+X[2]*X[3]*X[7]-X[0]*X[5]*X[7]-X[1]*X[3]*X[8]-X[2]*X[4]*X[6])//6)
