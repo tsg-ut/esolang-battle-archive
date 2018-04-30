05d1e8a642d71c2483f83f30314d42323d136362
moratorium08 <moratorium08@users.noreply.github.com>
2018-04-30 16:18:48 +0900

    Update x86asm-nasm.asm (1860 bytes)

--- 04/x86asm-nasm.asm
+++ 04/x86asm-nasm.asm
@@ -3,185 +3,141 @@ buf resb 200
 i resb 4
 a resb 40
 SECTION .text
-global main
 global get_int
 global write_int
+global main
 main:
-lea ecx, [esp+4H]
-and esp, 0FFFFFFF0H
+lea ecx,[esp+4H]
+and esp,0FFFFFFF0H
 push dword [ecx-4H]
 push ebp
-mov ebp, esp
+mov ebp,esp
+push edi
 push esi
 push ebx
 push ecx
-sub esp, 28
-mov dword [ebp-1CH], 0
-jmp ?_006
-?_004: call get_int
-mov edx, eax
-mov eax, dword [ebp-1CH]
-mov dword [a+eax*4], edx
-cmp dword [ebp-1CH], 2
-jle ?_005
-mov eax, dword [ebp-1CH]
-mov ebx, dword [a+eax*4]
-mov ecx, dword [ebp-1CH]
-mov edx, 1431655766
-mov eax, ecx
-imul edx
-mov eax, ecx
-sar eax, 31
-sub edx, eax
-mov eax, edx
-mov edx, eax
-add edx, edx
-add edx, eax
-mov eax, ecx
-sub eax, edx
-mov eax, dword [a+eax*4]
-sub ebx, eax
-mov edx, ebx
-mov eax, dword [ebp-1CH]
-mov dword [a+eax*4], edx
-?_005: add dword [ebp-1CH], 1
-?_006: cmp dword [ebp-1CH], 11
-jle ?_004
-mov eax, dword [a+0CH]
-mov ecx, dword [a+1CH]
-mov edx, dword [a+2CH]
-imul ecx, edx
-mov ebx, dword [a+20H]
-mov edx, dword [a+28H]
-imul edx, ebx
-sub ecx, edx
-mov edx, ecx
-imul edx, eax
-mov eax, dword [a+10H]
-mov ebx, dword [a+20H]
-mov ecx, dword [a+24H]
-imul ebx, ecx
-mov esi, dword [a+2CH]
-mov ecx, dword [a+18H]
-imul ecx, esi
-sub ebx, ecx
-mov ecx, ebx
-imul eax, ecx
-lea esi, [edx+eax]
-mov eax, dword [a+14H]
-mov ecx, dword [a+28H]
-mov edx, dword [a+18H]
-imul ecx, edx
-mov ebx, dword [a+1CH]
-mov edx, dword [a+24H]
-imul edx, ebx
-sub ecx, edx
-mov edx, ecx
-imul eax, edx
-lea ecx, [esi+eax]
-mov edx, 715827883
-mov eax, ecx
-imul edx
-mov eax, ecx
-sar eax, 31
-sub edx, eax
-mov eax, edx
-sub esp, 12
+sub esp,24
+mov byte [i],0
+?_002: movsx  ebx,byte [i]
+cmp bl,11
+jg  ?_003
+call get_int
+mov byte [a+ebx],al
+movsx  ebx,byte [i]
+mov cl,3
+movsx  ax,bl
+mov edx,ebx
+idiv cl
+cmp bl,2
+movsx  eax,ah
+movsx  ecx,al
+setg al
+inc edx
+mul byte [a+ecx]
+mov byte [i],dl
+sub byte [a+ebx],al
+jmp ?_002
+
+?_003: movsx  eax,byte [a+7H]
+movsx  ebx,byte [a+8H]
+sub esp,12
+movsx  edi,byte [a+0AH]
+movsx  esi,byte [a+0BH]
+movsx  ecx,byte [a+6H]
+mov dword [ebp-1CH],eax
+movsx  eax,byte [a+9H]
+mov edx,ebx
+imul edx,edi
+mov dword [ebp-20H],eax
+mov eax,dword [ebp-1CH]
+imul ebx,dword [ebp-20H]
+imul eax,esi
+sub eax,edx
+movsx  edx,byte [a+3H]
+imul esi,ecx
+imul ecx,edi
+sub ebx,esi
+movsx  esi,byte [a+4H]
+imul edx,eax
+mov eax,dword [ebp-1CH]
+imul eax,dword [ebp-20H]
+imul ebx,esi
+movsx  esi,byte [a+5H]
+add ebx,edx
+sub ecx,eax
+mov eax,ecx
+mov ecx,6
+imul eax,esi
+lea eax,[ebx+eax]
+cdq
+idiv ecx
+movzx  eax,ax
 push eax
 call write_int
-add esp, 16
-mov eax, 0
-lea esp, [ebp-0CH]
+add esp,16
+lea esp,[ebp-10H]
 pop ecx
 pop ebx
 pop esi
+pop edi
 pop ebp
-lea esp, [ecx-4H]
+lea esp,[ecx-4H]
 ret
 get_int:
 push ebp
-mov ebp, esp
-sub esp, 24
-mov dword [ebp-0CH], 0
-?_001: sub esp, 4
-push 1
-pop edx
-push buf
-pop ecx
-push 0
-pop ebx
-push 3
-pop eax
+mov ebp,esp
+sub esp,12
+push edx
+push ecx
+push ebx
+mov edx,3
+mov ecx,buf
+mov ebx,0
+mov eax,3
 int 0x80
-add esp, 16
-movzx eax, byte [buf]
-cmp al, 10
-jz ?_002
-movzx eax, byte [buf]
-cmp al, 32
-jz ?_002
-mov edx, dword [ebp-0CH]
-mov eax, edx
-shl eax, 2
-add eax, edx
-add eax, eax
-mov edx, eax
-movzx eax, byte [buf]
-movsx eax, al
-sub eax, 48
-add eax, edx
-mov dword [ebp-0CH], eax
-jmp ?_001
-?_002: mov eax, dword [ebp-0CH]
+pop ebx
+pop ecx
+pop edx
+movsx  eax,byte [buf]
+mov dl,10
+sub eax,48
+imul eax,edx
+mov dl,byte [buf+1H]
 leave
+lea eax,[eax+edx-30H]
 ret
 write_int:
 push ebp
-mov ebp, esp
-sub esp, 8
-cmp dword [ebp+8H], 0
-jle ?_003
-mov ecx, dword [ebp+8H]
-mov edx, 1717986919
-mov eax, ecx
-imul edx
-sar edx, 2
-mov eax, ecx
-sar eax, 31
-sub edx, eax
-mov eax, edx
-sub esp, 12
+mov ebp,esp
+push ebx
+push eax
+mov edx,dword [ebp+8H]
+test dx,dx
+jz  ?_001
+mov ecx,10
+mov eax,edx
+xor edx,edx
+div cx
+sub esp,12
+movzx  eax,ax
+mov ebx,edx
 push eax
 call write_int
-add esp, 16
-mov ecx, dword [ebp+8H]
-mov edx, 1717986919
-mov eax, ecx
-imul edx
-sar edx, 2
-mov eax, ecx
-sar eax, 31
-sub edx, eax
-mov eax, edx
-shl eax, 2
-add eax, edx
-add eax, eax
-sub ecx, eax
-mov edx, ecx
-mov eax, edx
-add eax, 48
-mov byte [buf], al
-sub esp, 4
-push 1
-pop edx
-push buf
-pop ecx
-push 1
-pop ebx
-push 4
-pop eax
+add esp,12
+lea eax,[ebx+30H]
+mov byte [buf],al
+push edx
+push ecx
+push ebx
+mov edx,1
+mov ecx,buf
+mov ebx,1
+mov eax,4
 int 0x80
-add esp, 16
-?_003: nop
+pop ebx
+pop ecx
+pop edx
+add esp,16
+?_001: mov ebx,dword [ebp-4H]
 leave
 ret
