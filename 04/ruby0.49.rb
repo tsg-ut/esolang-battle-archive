14795a84f2307313cb99d6b83b813b74c1f1530e
cookie-s <cookie-s@users.noreply.github.com>
2018-05-01 05:12:40 +0900

    Update ruby0.49.rb (136 bytes)

--- 04/ruby0.49.rb
+++ 04/ruby0.49.rb
@@ -1,25 +1,5 @@
-x=[]
-i=0
-while gets()
-    for w in $_.split('/\s/')
-    x[i]=w.split(" ")
-    end
-    i=i+1
-end
-p=x[0]
-q=x[1]
-r=x[2]
-s=x[3]
-j=p[0].to_i
-k=p[1].to_i
-l=p[2].to_i
-a=q[0].to_i-j
-b=q[1].to_i-k
-c=q[2].to_i-l
-d=r[0].to_i-j
-e=r[1].to_i-k
-f=r[2].to_i-l
-g=s[0].to_i-j
-h=s[1].to_i-k
-i=s[2].to_i-l
-print ((a*e*i+b*f*g+c*d*h-a*f*h-b*d*i-c*e*g)/6)
+print(system2("bash -c 'a=(`cat`)
+for((;z<9;)){
+$[a[t=z]-=a[9+t++%3],s+=z/6*a[z++]*(a[p=t++%3+3]*a[t%=3]-a[t+3]*a[p-3])/6]
+}
+echo $s'"))
