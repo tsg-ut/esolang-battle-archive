9f3524708ae5134be0411a35780c6a9a08364a46
Yosshi999 <Yosshi999@users.noreply.github.com>
2018-04-30 11:02:40 +0900

    Update python3.py (141 bytes)

--- 04/python3.py
+++ 04/python3.py
@@ -1,3 +1,3 @@
-X=[*map(int,open(0).read().split())]
-a,b,c,d,e,f,g,h,i=[int(b)-a for a,b in zip(X[0:3]*3,X[3:])]
+s=[*map(int,open(0).read().split())]
+j,k,l,a,b,c,d,e,f,g,h,i=[s[i]-s[i%3] for i in range(12)]
 print((a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)//6)
