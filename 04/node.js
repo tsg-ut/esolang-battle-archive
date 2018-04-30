8666b4bec84ebccc00572fef97b31f7160342538
yynet21 <yynet21@users.noreply.github.com>
2018-04-30 12:34:32 +0900

    Update node.js (148 bytes)

--- 04/node.js
+++ 04/node.js
@@ -1,1 +1,1 @@
-let[,,,a,b,c,d,e,f,g,h,i]=(""+require("fs").readFileSync(0)).split(/\s/).map((e,i,x)=>e/1-x[i%3]);console.log((a*e*i+b*f*g+c*d*h-a*f*h-b*d*i-c*e*g)/6)
+let[,,,a,b,c,d,e,f,g,h,i]=(""+require("fs").readFileSync(0)).split(/\s/).map((e,i,x)=>e-x[i%3]);console.log((a*e*i+b*f*g+c*d*h-a*f*h-b*d*i-c*e*g)/6)
