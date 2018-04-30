1dc1efbfc3bdce7ad327a49609dde0c0b97d0280
kurgm <kurgm@users.noreply.github.com>
2018-04-30 19:33:51 +0900

    Update crystal.cr (118 bytes)

--- 04/crystal.cr
+++ 04/crystal.cr
@@ -1,1 +1,1 @@
-Y=`dd`.split;r,t,y,a,b,c,d,e,f,g,h,i=Y.map_with_index{|e,i|e.to_i-Y[i%3].to_i};p (a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6
+i=-1;Y=`dd`.split;r,t,y,a,b,c,d,e,f,g,h,i=Y.map{|e|i+=1;e.to_i-Y[i%3].to_i};p (a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6
