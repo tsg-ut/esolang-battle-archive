8cf0e608440a7c48a2a9c62034361c5628ce5155
akouryy <akouryy@users.noreply.github.com>
2018-05-01 00:59:43 +0900

    Update fish.fish (184 bytes)

--- 04/fish.fish
+++ 04/fish.fish
@@ -1,9 +1,3 @@
-c>1-:f ia*i+68b**-i~v
-v^?:p@              <
->0fg3fg-3fp 0fg6fg-6fp 0fg9fg-9fpv
-v                                <
->1fg4fg-4fp 1fg7fg-7fp 1fgafg-afpv
-v                                <
->2fg5fg-5fp 2fg8fg-8fp 2fgbfg-bfpv
-v                                <
->7fgbfg* 8fgafg* -3fg* 8fg9fg* 6fgbfg* -4fg* 6fgafg* 7fg9fg* -5fg* ++ 6,n;
+c>1-:fia*i+68v
+v^?:p@~i-**b <
+>0fg3fg-3fp0fg6fg-6fp0fg9fg-9fp1fg4fg-4fp1fg7fg-7fp1fgafg-afp2fg5fg-5fp2fg8fg-8fp2fgbfg-bfp7fgbfg*8fgafg*-3fg*8fg9fg*6fgbfg*-4fg*6fgafg*7fg9fg*-5fg*++6,n;
