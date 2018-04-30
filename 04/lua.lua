0ff4c48e6122f7f7a0a274c9cf9f8e61e11163c0
kurgm <kurgm@users.noreply.github.com>
2018-05-01 01:32:55 +0900

    Update lua.lua (137 bytes)

--- 04/lua.lua
+++ 04/lua.lua
@@ -1,5 +1,8 @@
-s=0
-a={}for k=12,1,-1 do
-a[k]=io.read("*n")-(a[10+~-k%3]or 0);q=-~k%3
-s=s+a[k]*(k<4 and a[4+k%3]*a[7+q]-a[7+k%3]*a[4+q]or 0)end
-print(s//6)
+X={}
+x=0
+for i=-3,8 do
+X[i]=io.read("*n")-(X[i%3-3]or 0)if i>5 then p=-~i%3
+q=~-i%3
+x=x+X[i]*(X[p]*X[q+3]-X[q]*X[p+3])end
+end
+print(x//6)
