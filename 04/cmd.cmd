a84a00754c0eff56b0e85a5a0423ad3a7c58ff04
Yosshi999 <Yosshi999@users.noreply.github.com>
2018-04-30 15:57:17 +0900

    Update cmd.cmd (268 bytes)

--- 04/cmd.cmd
+++ 04/cmd.cmd
@@ -1,13 +1,13 @@
 set/p q=
 for /f "tokens=1-12 delims=%q:~8,1% " %%a in ("%q%") do (
-set /a a=%%d-%%a
-set /a b=%%e-%%b
-set /a c=%%f-%%c
-set /a d=%%g-%%a
-set /a e=%%h-%%b
-set /a f=%%i-%%c
-set /a g=%%j-%%a
-set /a h=%%k-%%b
-set /a i=%%l-%%c)
-set /a v=(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6
+set/a a=%%d-%%a
+set/a b=%%e-%%b
+set/a c=%%f-%%c
+set/a d=%%g-%%a
+set/a e=%%h-%%b
+set/a f=%%i-%%c
+set/a g=%%j-%%a
+set/a h=%%k-%%b
+set/a i=%%l-%%c)
+set/a v=(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6
 echo %v%
