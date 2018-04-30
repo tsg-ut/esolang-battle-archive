9929fe8452849cd11aec461a593754444d7b34e8
gasin <gasin@users.noreply.github.com>
2018-04-30 10:36:48 +0900

    Update c-gcc.c (198 bytes)

--- 04/c-gcc.c
+++ 04/c-gcc.c
@@ -1,1 +1,2 @@
-a,b,c,d,e,f,g,h,i,j,k,l;main(){scanf("%d%d%d%d%d%d%d%d%d%d%d%d",&j,&k,&l,&a,&b,&c,&d,&e,&f,&g,&h,&i);a-=j;b-=k;c-=l;d-=j;e-=k;f-=l;g-=j;h-=k;i-=l;printf("%d",(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6);}
+a,b,c,d,e,f,g,h,i,j,k;
+main(l){scanf("%d%d%d%d%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l);g-=a;h-=b;i-=c;j-=a;k-=b;l-=c;printf("%d",((e-b)*(i*j-g*l)+(f-c)*(g*k-h*j)+(d-a)*(h*l-i*k))/6);}
