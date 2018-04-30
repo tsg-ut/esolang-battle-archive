f51e9c92412fd06f09a5c64289883e3d7f1f7cca
Yosshi999 <Yosshi999@users.noreply.github.com>
2018-04-30 15:40:05 +0900

    Update cmd.cmd (316 bytes)

--- 04/cmd.cmd
+++ 04/cmd.cmd
@@ -1,15 +1,14 @@
 set /p q=
-set j=%q:~0,2%
-set k=%q:~3,2%
-set l=%q:~6,2%
-set /a a=%q:~9,2%-%j%
-set /a b=%q:~12,2%-%k%
-set /a c=%q:~15,2%-%l%
-set /a d=%q:~18,2%-%j%
-set /a e=%q:~21,2%-%k%
-set /a f=%q:~24,2%-%l%
-set /a g=%q:~27,2%-%j%
-set /a h=%q:~30,2%-%k%
-set /a i=%q:~33,2%-%l%
-set /a v="(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6"
+for /f "tokens=1-12 delims=%q:~8,1% " %%a in ("%q%") do (
+set /a a=%%d-%%a
+set /a b=%%e-%%b
+set /a c=%%f-%%c
+set /a d=%%g-%%a
+set /a e=%%h-%%b
+set /a f=%%i-%%c
+set /a g=%%j-%%a
+set /a h=%%k-%%b
+set /a i=%%l-%%c
+)
+set /a v=(%a%*%e%*%i%+%b%*%f%*%g%+%c%*%d%*%h%-%a%*%h%*%f%-%b%*%i%*%d%-%c%*%g%*%e%)/6
 echo %v%
