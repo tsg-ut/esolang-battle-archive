f9193090591891bd297e18d2bdb9515ac612e93a
yynet21 <yynet21@users.noreply.github.com>
2018-04-30 11:44:24 +0900

    Update node.js (162 bytes)

--- 04/node.js
+++ 04/node.js
@@ -1,3 +1,3 @@
-q=(""+require("fs").readFileSync(0)).split(/\s/).map(i=>parseInt(i))
+q=(""+require("fs").readFileSync(0)).split(/\s/).map(i=>i|0)
 let[,,,a,b,c,d,e,f,g,h,i]=q.map((e,i)=>e-q[i%3])
 console.log((a*e*i+b*f*g+c*d*h-a*f*h-b*d*i-c*e*g)/6)
