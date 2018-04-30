cffdd553fc7b34927e5e62f2105cf08bd2d07c1e
Yosshi999 <Yosshi999@users.noreply.github.com>
2018-04-30 14:48:28 +0900

    Update cmd.cmd (363 bytes)

--- 04/cmd.cmd
+++ 04/cmd.cmd
@@ -1,16 +1,15 @@
-set /p line=
-set /a j=%line:~0,2%
-set /a k=%line:~3,2%
-set /a l=%line:~6,2%
-set /a a=%line:~9,2% - %j%
-set /a b=%line:~12,2% - %k%
-set /a c=%line:~15,2% - %l%
-set /a d=%line:~18,2% - %j%
-set /a e=%line:~21,2% - %k%
-set /a f=%line:~24,2% - %l%
-set /a g=%line:~27,2% - %j%
-set /a h=%line:~30,2% - %k%
-set /a i=%line:~33,2% - %l%
+set /p q=
+set /a j=%q:~0,2%
+set /a k=%q:~3,2%
+set /a l=%q:~6,2%
+set /a a=%q:~9,2%-%j%
+set /a b=%q:~12,2%-%k%
+set /a c=%q:~15,2%-%l%
+set /a d=%q:~18,2%-%j%
+set /a e=%q:~21,2%-%k%
+set /a f=%q:~24,2%-%l%
+set /a g=%q:~27,2%-%j%
+set /a h=%q:~30,2%-%k%
+set /a i=%q:~33,2%-%l%
 set /a v=(%a%*%e%*%i%+%b%*%f%*%g%+%c%*%d%*%h%-%a%*%h%*%f%-%b%*%i%*%d%-%c%*%g%*%e%)/6
-
 echo %v%
