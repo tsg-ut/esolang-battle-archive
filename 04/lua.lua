0cdf59b65c1b32fe63d9b247c2b88098fc988ec7
kurgm <kurgm@users.noreply.github.com>
2018-04-30 15:05:31 +0900

    Update lua.lua (157 bytes)

--- 04/lua.lua
+++ 04/lua.lua
@@ -1,5 +1,3 @@
-a={}for k=1,12 do a[k]=io.read("*n")end
-for z=1,9 do a[z]=a[z]-a[10+~-z%3]end
-s=0
-for z=1,3 do q=-~z%3;s=s+a[z]*(a[7+z%3]*a[4+q]-a[4+z%3]*a[7+q])end
-print(math.ceil(s/6))
+X={}for i=-3,8 do X[i]=0
+X[i]=io.read("*n")-X[i%3-3]end
+print((X[0]*X[4]*X[8]+X[1]*X[5]*X[6]+X[2]*X[3]*X[7]-X[0]*X[5]*X[7]-X[1]*X[3]*X[8]-X[2]*X[4]*X[6])//6)
