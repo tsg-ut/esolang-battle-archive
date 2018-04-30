3a70851d7f572d3bd03db347e81d966570deee1d
Liesegang <Liesegang@users.noreply.github.com>
2018-04-30 11:01:15 +0900

    Update php.php (270 bytes)

--- 04/php.php
+++ 04/php.php
@@ -1,1 +1,1 @@
-<?php [$a,$b,$c]=explode(" ",fgets(STDIN));[$d,$e,$f]=explode(" ",fgets(STDIN));[$g,$h,$i]=explode(" ",fgets(STDIN));[$j,$k,$l]=explode(" ",fgets(STDIN));$g-=$a;$h-=$b;$i-=$c;$j-=$a;$k-=$b;$l-=$c;echo (($e-$b)*($i*$j-$g*$l)+($f-$c)*($g*$k-$h*$j)+($d-$a)*($h*$l-$i*$k))/6;
+<?php [$a,$b,$c]=explode(" ",fgets(STDIN));[$d,$e,$f]=explode(" ",fgets(STDIN));[$g,$h,$i]=explode(" ",fgets(STDIN));[$j,$k,$l]=explode(" ",fgets(STDIN));$g-=$a;$h-=$b;$i-=$c;$j-=$a;$k-=$b;$l-=$c;echo(($e-$b)*($i*$j-$g*$l)+($f-$c)*($g*$k-$h*$j)+($d-$a)*($h*$l-$i*$k))/6;
