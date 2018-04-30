45cc58e76d0430d5f8492703cc51f65bb97e57de
Omikemike <Omikemike@users.noreply.github.com>
2018-04-30 14:12:02 +0900

    Update verilog.v (231 bytes)

--- 04/verilog.v
+++ 04/verilog.v
@@ -1,1 +1,1 @@
-module z;integer a,b,c,d,e,f,g,h,i,j,k,l,z;initial begin z=$fscanf(1<<31,"%d%d%d%d%d%d%d%d%d%d%d%d",a,b,c,d,e,f,g,h,i,j,k,l);d=d-a;e=e-b;f-=c;g=g-a;h=h-b;i=i-c;j=j-a;k=k-b;l=l-c;$write((g*k*f+j*e*i+d*h*l-j*h*f-d*k*i-g*e*l)/6);end endmodule
+module z;integer a,b,c,d,e,f,g,h,i,j,k,l,z;initial begin z=$fscanf(1<<31,"%d%d%d%d%d%d%d%d%d%d%d%d",a,b,c,d,e,f,g,h,i,j,k,l);d-=a;e-=b;f-=c;g-=a;h-=b;i-=c;j-=a;k-=b;l-=c;$write((g*k*f+j*e*i+d*h*l-j*h*f-d*k*i-g*e*l)/6);end endmodule
