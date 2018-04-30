8e650de6f6f2e73e2892b1554442ee20f2120385
gasin <gasin@users.noreply.github.com>
2018-04-30 14:41:14 +0900

    Update lua.lua (212 bytes)

--- 04/lua.lua
+++ 04/lua.lua
@@ -1,5 +1,5 @@
 a={}for k=1,4 do for s in string.gmatch(io.read(),"%w+")do a[#a+1]=s end end
 for z=1,9 do a[z]=a[z]-a[10+~-z%3]end
 s=0
-for z=0,2 do p=-~z%3;q=(z+2)%3;s=s+a[z+1]*(a[7+p]*a[4+q]-a[4+p]*a[7+q])end
+for z=1,3 do p=z%3;q=(z+1)%3;s=s+a[z]*(a[7+p]*a[4+q]-a[4+p]*a[7+q])end
 print(math.floor(s/6))
