4ce8e821eb0bbc80df46c2095cd1da62896257f1
Liesegang <Liesegang@users.noreply.github.com>
2018-05-01 00:41:17 +0900

    Update bash-busybox.sh (124 bytes)

--- 04/bash-busybox.sh
+++ 04/bash-busybox.sh
@@ -1,5 +1,6 @@
-read j k l
-read a b c
-read d e f
-read g h i
-echo $[a-=j,b-=k,c-=l,d-=j,e-=k,f-=l,g-=j,h-=k,i-=l,(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6]
+a=(`cat -`)
+for z in {0..8};{
+t=$[a[z]-=a[9+z%3]]
+s=$[s+=z/6*a[z]*(a[p=3+(z+1)%3]*a[q=(z+2)%3]-a[q+3]*a[p-3])]
+}
+echo $[s/6]
