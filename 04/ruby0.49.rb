4c84ff5f9226ff469ecff2561195e20a02a301c9
yynet21 <yynet21@users.noreply.github.com>
2018-04-30 17:01:06 +0900

    Update ruby0.49.rb (415 bytes)

--- 04/ruby0.49.rb
+++ 04/ruby0.49.rb
@@ -1,14 +1,12 @@
-freq = {}
 x=[]
 i=0
 while gets()
-    for word in $_.split('/\s/')
-a=word.split(" ")
+    for w in $_.split('/\s/')
+    a=w.split(" ")
     end
 x[i]=a 
 i=i+1
 end
-print("")
 a=x[1][0].to_i - x[0][0].to_i
 b=x[1][1].to_i - x[0][1].to_i
 c=x[1][2].to_i - x[0][2].to_i
@@ -19,4 +17,3 @@ g=x[3][0].to_i - x[0][0].to_i
 h=x[3][1].to_i - x[0][1].to_i
 i=x[3][2].to_i - x[0][2].to_i
 print ((a*e*i+b*f*g+c*d*h-a*f*h-b*d*i-c*e*g)/6)
-   
