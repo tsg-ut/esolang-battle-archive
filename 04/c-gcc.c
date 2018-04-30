b32977699ab1878996924628a6bca399855ec25c
akouryy <akouryy@users.noreply.github.com>
2018-04-30 11:43:45 +0900

    Update c-gcc.c (161 bytes)

--- 04/c-gcc.c
+++ 04/c-gcc.c
@@ -1,1 +1,1 @@
-a[12];s,p,q;main(y,z){z=12;for(;z--;)scanf("%d",a+z);for(z=9;z--;)a[z]-=a[9+z%3];for(z=3;z--;)s+=a[z]*=a[p=3+-~z%3]*a[q=6+(z+2)%3]-a[q-3]*a[p+3];printf("%d",s/6);}
+a[12];s,q;main(p,z){z=12;for(;z--;)scanf("%d",a+z);for(z=9;z--;)a[z]-=a[9+z%3];for(z=3;z--;)s+=a[z]*=a[p=3+-~z%3]*a[q=6+(z+2)%3]-a[q-3]*a[p+3];printf("%d",s/6);}
