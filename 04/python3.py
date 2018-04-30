b272429e2d852fa26a5c1745f493c90fcc3a4902
kurgm <kurgm@users.noreply.github.com>
2018-04-30 10:58:36 +0900

    Update python3.py (144 bytes)

--- 04/python3.py
+++ 04/python3.py
@@ -1,11 +1,3 @@
-j,k,l,a,b,c,d,e,f,g,h,i=map(int,open(0).read().split())
-a-=j
-b-=k
-c-=l
-d-=j
-e-=k
-f-=l
-g-=j
-h-=k
-i-=l
+X=[*map(int,open(0).read().split())]
+a,b,c,d,e,f,g,h,i=[int(b)-a for a,b in zip(X[0:3]*3,X[3:])]
 print((a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)//6)
