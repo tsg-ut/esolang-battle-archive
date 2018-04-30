a9fe707ee39a528ddc3dcdbdba455dbfbdaa1b41
kurgm <kurgm@users.noreply.github.com>
2018-05-01 00:30:46 +0900

    Update cmd.cmd (209 bytes)

--- 04/cmd.cmd
+++ 04/cmd.cmd
@@ -1,4 +1,3 @@
 set/pq=
-for /f "tokens=1-12 delims=%q:~8,1% " %%a in ("%q%") do set/aa=%%d-%%a,b=%%e-%%b,c=%%f-%%c,d=%%g-%%a,e=%%h-%%b,f=%%i-%%c,g=%%j-%%a,h=%%k-%%b,i=%%l-%%c
-set/av=(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6
+for /f "tokens=1-12 delims=%q:~8,1% " %%a in ("%q%") do set/aa=%%d-%%a,b=%%e-%%b,c=%%f-%%c,d=%%g-%%a,e=%%h-%%b,f=%%i-%%c,g=%%j-%%a,h=%%k-%%b,i=%%l-%%c,v=(a*e*i+b*f*g+c*d*h-a*h*f-b*i*d-c*g*e)/6
 echo %v%
