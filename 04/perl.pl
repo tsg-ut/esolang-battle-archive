e0c584b9ac4568a4de6f792b588dc1105f237ca4
akouryy <akouryy@users.noreply.github.com>
2018-04-30 10:26:31 +0900

    Update perl.pl (199 bytes)

---
+++ 04/perl.pl
@@ -0,0 +1,1 @@
+use Data::Dumper;my($a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l)=map{split' '}<>;$d-=$a;$e-=$b;$f-=$c;$g-=$a;$h-=$b;$i-=$c;$j-=$a;$k-=$b;$l-=$c;print+($g*$k*$f+$j*$e*$i+$d*$h*$l-$j*$h*$f-$d*$k*$i-$g*$e*$l)/6
