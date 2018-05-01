92f54b0e7ad92ed360677a6428d7d903b22abc48
n4o847 <n4o847@users.noreply.github.com>
2018-05-02 02:42:38 +0900

    Update python3.py (125 bytes)

--- 04/python3.py
+++ 04/python3.py
@@ -1,2 +1,2 @@
-a=[*map(int,open(0).read().split())]
-print(-sum(a[i//2]*a[8-i//12*6-i%3]*a[(5<i<18)*6+3+(i+3)//2%3]^-(i%2)for i in range(24))//6-2)
+x,y,z,*s=map(int,open(0).read().split())
+print(sum((s[i]-x)*((s[i-5]-y)*(s[i-1]-z)-(s[i-2]-y)*(s[i-4]-z))for i in[0,3,6])//6)
