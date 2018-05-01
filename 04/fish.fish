7dd14a87872dccfe342c5da419426196ef0d602b
akouryy <akouryy@users.noreply.github.com>
2018-05-01 19:39:08 +0900

    Update fish.fish (129 bytes)

--- 04/fish.fish
+++ 04/fish.fish
@@ -1,5 +1,5 @@
 c>1-:fia*iv
 v^?:p@~i+ <
->3>1-::3+:fg@@fg-$fp::6+:fg@v
-v8^?:pf$-gf@@gf:+9::pf$-gf@ <
+>2>:fgffp>3+::fgffgv
+v8^?+1:-a^?(9:pf$- <
 >fgafg*7fgbfg*-3fg*6fgbfg*8fg9fg*-4fg*7fg9fg*6fgafg*-5fg*++6,n;
