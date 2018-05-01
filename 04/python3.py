93cad79334754fd986f096385af932e3f93dd244
satos---jp <satos---jp@users.noreply.github.com>
2018-05-02 01:36:36 +0900

    Update python3.py (131 bytes)

--- 04/python3.py
+++ 04/python3.py
@@ -1,2 +1,2 @@
 a=[*map(int,open(0).read().split())]
-print(-sum(a[i//2]*a[8-i//12*6-i%3]*a[[3,9][5<i<18]+(i+3)//2%3]^-(i%2)for i in range(24))//6-2)
+print(-sum(a[i//2]*a[8-i//12*6-i%3]*a[(5<i<18)*6+3+(i+3)//2%3]^-(i%2)for i in range(24))//6-2)
