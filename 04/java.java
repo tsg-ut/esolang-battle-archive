34adf282b866bd331cae36978df3ebe53d926080
kurgm <kurgm@users.noreply.github.com>
2018-05-02 12:57:48 +0900

    Update java.java (233 bytes)

--- 04/java.java
+++ 04/java.java
@@ -1,1 +1,1 @@
-import java.util.*;interface A{static void main(String[]a){int i=0,x=0,p,q,X[]=new int[12];for(Scanner s=new Scanner(System.in);i<12;x+=i/9*X[i]*(X[p=++i%3+3]*X[q=-~i%3+6]-X[p+3]*X[q-3]))X[i]=s.nextInt()-X[i%3];System.out.print(x/6);}}
+import java.util.*;interface A{static void main(String[]a){int i=0,x=0,p,q,X[]=new int[12];for(Scanner s=new Scanner(System.in);i<12;)x+=i/9*(X[i]=s.nextInt()-X[i%3])*(X[p=++i%3+3]*X[q=-~i%3+6]-X[p+3]*X[q-3]);System.out.print(x/6);}}
