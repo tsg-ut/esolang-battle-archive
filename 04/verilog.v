7168394e63cd664e67e2c497a2d461a6ea5c4d18
kurgm <kurgm@users.noreply.github.com>
2018-05-02 22:22:05 +0900

    Update verilog.v (193 bytes)

--- 04/verilog.v
+++ 04/verilog.v
@@ -1,3 +1,3 @@
 module m;integer X[0:11],v,s;initial begin
-for(v=0;$fscanf(1<<31,"%d",X[v]);v+=1)X[v]-=v>2?X[v%3]:0;for(s=0;v>9;v-=1)s+=X[v-1]*(X[v%3+3]*X[(v+1)%3+6]-X[v%3+6]*X[(v+1)%3+3]);$write(s/6);end
+for(v=0;$fscanf(1<<31,"%d",X[v]);v+=1)X[v-3]-=X[v];s=0;for(v=11;v>2;v-=3)s+=X[v-5]*(X[v%9]*X[(v+2)%9]-X[(v-1)%9]*X[(v+3)%9]);$write(s/6);end
 endmodule
