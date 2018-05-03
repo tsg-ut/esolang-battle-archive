a5cb123417f071a1bbc9ead90e7e4bfcf81148c8
kurgm <kurgm@users.noreply.github.com>
2018-05-04 03:28:08 +0900

    Update kotlin.kt (215 bytes)

--- 04/kotlin.kt
+++ 04/kotlin.kt
@@ -3,7 +3,5 @@ fun main(a:Array<String>){var x=0
 val X=IntArray(12)
 val s=Scanner(System.`in`)
 repeat(12){i->X[i]=s.nextInt()-X[i%3]
-val p=(i+1)%3+3
-val q=(i+2)%3+6
-x+=i/9*X[i]*(X[p]*X[q]-X[p+3]*X[q-3])}
+x+=i/9*X[i]*(X[(i+1)%3+3]*X[(i+2)%3+6]-X[(i+1)%3+6]*X[(i+2)%3+3])}
 print(x/6)}
