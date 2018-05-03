72200481c762475ca8924caff12ed5f7da840db1
kurgm <kurgm@users.noreply.github.com>
2018-05-04 03:27:19 +0900

    Update kotlin.kt (219 bytes)

--- 04/kotlin.kt
+++ 04/kotlin.kt
@@ -1,3 +1,9 @@
-fun main(b:Array<String>){val a=(0..3).flatMap{readLine()!!.split(" ").map{a->a.toInt()}}.toMutableList()
-for(z in 0..8)a[z]-=a[9+z%3]
-print((a[7]*(a[0]*a[5]-a[3]*a[2])+a[6]*(a[4]*a[2]-a[1]*a[5])+a[8]*(a[3]*a[1]-a[0]*a[4]))/6)}
+import java.util.*
+fun main(a:Array<String>){var x=0
+val X=IntArray(12)
+val s=Scanner(System.`in`)
+repeat(12){i->X[i]=s.nextInt()-X[i%3]
+val p=(i+1)%3+3
+val q=(i+2)%3+6
+x+=i/9*X[i]*(X[p]*X[q]-X[p+3]*X[q-3])}
+print(x/6)}
