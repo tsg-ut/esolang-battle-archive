56c2d78b13d9b8520ec225f796575c83197079f6
kurgm <kurgm@users.noreply.github.com>
2018-05-04 10:23:45 +0900

    Update kotlin.kt (205 bytes)

--- 04/kotlin.kt
+++ 04/kotlin.kt
@@ -1,7 +1,5 @@
 import java.util.*
-fun main(a:Array<String>){var x=0
+fun main(a:Array<String>){val s=Scanner(System.`in`)
 val X=IntArray(12)
-val s=Scanner(System.`in`)
-repeat(12){i->X[i]=s.nextInt()-X[i%3]
-x+=i/9*X[i]*(X[(i+1)%3+3]*X[(i+2)%3+6]-X[(i+1)%3+6]*X[(i+2)%3+3])}
-print(x/6)}
+print((0..11).sumBy{i->X[i]=s.nextInt()-X[i%3]
+i/9*X[i]*(X[(i+1)%3+3]*X[(i+2)%3+6]-X[(i+1)%3+6]*X[(i+2)%3+3])}/6)}
