e9e47347d17597ef08b53ab982109927991698df
yynet21 <yynet21@users.noreply.github.com>
2018-05-04 12:58:15 +0900

    Update swift.swift (216 bytes)

--- 04/swift.swift
+++ 04/swift.swift
@@ -1,8 +1,4 @@
 var x:[Int]=[]
-for _ in 0..<4{
-x+=readLine()!.characters.split{$0==" "}.map{Int(String($0))!}
-}
-for i in 0..<9{
-x[i]-=x[9+i%3]
-}
+for _ in 0..<4{x+=readLine()!.characters.split{$0==" "}.map{Int(String($0))!}}
+for i in 0..<9{x[i]-=x[9+i%3]}
 print((x[0]*(x[5]*x[7]-x[4]*x[8])+x[1]*(x[3]*x[8]-x[5]*x[6])+x[2]*(x[4]*x[6]-x[3]*x[7]))/6)
