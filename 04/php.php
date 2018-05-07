b4151411e81f82e7963be53e40acae2ce88b242a
kurgm <kurgm@users.noreply.github.com>
2018-05-07 23:49:59 +0900

    Update php.php (165 bytes)

--- 04/php.php
+++ 04/php.php
@@ -1,2 +1,2 @@
 <?php
-for($x=[];$i<4;$i++)$x=array_merge($x,fgetcsv(STDIN,9," "));for($i=0;$i<9;)$a+=($i>5)*($x[$i]-=$x[$i+3])*($x[3-~$i%3]*$x[$t=~-$i%3]-$x[++$i%3]*$x[$t+3]);echo$a/6;
+for($x=[],$i=5;--$i;)$x=array_merge($x,fgetcsv(STDIN,9," "));for(;$i<9;)$a+=($i>5)*($x[$i]-=$x[$i+3])*($x[3-~$i%3]*$x[$t=~-$i%3]-$x[++$i%3]*$x[$t+3]);echo$a/6;
