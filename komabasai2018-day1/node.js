8dff7c1bdacbc967fa94d1d6a059d25d2e34cbee
n4o847 <n4o847@users.noreply.github.com>
2018-11-23 15:16:37 +0900

    Update node.js (122 bytes)

--- komabasai2018-day1/node.js
+++ komabasai2018-day1/node.js
@@ -1,7 +1,6 @@
 [h,w]=(""+require("fs").readFileSync(0)).split`
 `
-s=a="*".repeat(w)+`
+a="*".repeat(w)+`
 `
-s+=`*${" ".repeat(w-2)}*
-`.repeat(h-2)
-console.log(s+a)
+console.log(a+`*${" ".repeat(w-2)}*
+`.repeat(h-2)+a)
