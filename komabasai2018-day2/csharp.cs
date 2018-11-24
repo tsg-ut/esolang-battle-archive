e22e57c9255e7277afc2759f09acb28b165402df
akouryy <akouryy@users.noreply.github.com>
2018-11-24 15:06:58 +0900

    Update csharp.cs (177 bytes)

--- komabasai2018-day2/csharp.cs
+++ komabasai2018-day2/csharp.cs
@@ -1,10 +1,1 @@
-class M{
-static void Main(string[]a){
-string s = System.Console.ReadLine();
-for(int i = 0; i < 50; i++)
-  if(i < 33 && s[i] == '1' && s[i+5] == '1' && s[i+12] == '1' && s[i+17] == '1') 
-    System.Console.Write(1);
-  else
-    System.Console.Write(0);
-}
-}
+class M{static void Main(string[]a){string s=System.Console.ReadLine();for(int i=0;i<50;i++)System.Console.Write(i<33&&s[i]=='1'&&s[i+5]=='1'&&s[i+12]=='1'&&s[i+17]=='1'?1:0);}}
