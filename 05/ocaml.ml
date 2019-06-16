7033ccffb231c011423b13c4554c369e037e805d
utgwkk <utgwkk@users.noreply.github.com>
2019-06-16 14:16:39 +0900

    Update ocaml.ml (231 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -1,1 +1,5 @@
-let p=print_endline open String open List let rec r x=try r(read_line()::x)with _->x let _=r[]|>rev|>fold_left(fun l s->if l=0 then(p s;index s 'T')else(p(try(let i=index s 'K'in make i ' '^"K"^make(l-i)'*')with _->make l ' '^"*");l))0
+open List
+open String
+let p=print_endline
+let rec r x=try r(read_line()::x)with _->x
+let _=fold_right(fun s l->if l=0 then(p s;index s 'T')else(p(try(let i=index s 'K'in make i ' '^"K"^make(l-i)'*')with _->make l ' '^"*");l))(r[])0
