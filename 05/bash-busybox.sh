87217428d56cb7849fdb6eb928c98689c04e3082
cookie-s <cookie-s@users.noreply.github.com>
2019-06-16 16:18:15 +0900

    Update bash-busybox.sh (167 bytes)

--- 05/bash-busybox.sh
+++ 05/bash-busybox.sh
@@ -1,7 +1,7 @@
 IFS= read a;while IFS= read b;do
 echo "$a";f="$b";g="$f"
 done
-c=`sed -r 's/T.+/T/'<<<"$a"`
-d=`sed -r 's/K.+/K/'<<<"$g"`
+c=`sed -r s/T.+/T/<<<$a`
+d=`sed -r s/K.+/K/<<<$g`
 printf "$d"
 printf *%.s `eval echo {1..$[${#c}-${#d}]}`
