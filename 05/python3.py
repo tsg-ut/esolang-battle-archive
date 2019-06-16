8c2a24fe8df8164f0d0de0ca80b97050aee2d7f4
drafear <drafear@users.noreply.github.com>
2019-06-16 14:19:18 +0900

    Update python3.py (91 bytes)

--- 05/python3.py
+++ 05/python3.py
@@ -1,3 +1,6 @@
-i=input().find('T')
-d=-1
-while 1:print(['*'*(i-d+1),'*'][d<0].rjust(i+1));d=input().find('K')
+a,*b,c=open(0)
+i=a.find("T")
+x=a
+for l in b:x+=a
+j=c.find("K")
+print(x+" "*j+"K"+"*"*(i-j))
