522d16fbef42c3e40393752fd251bca144722270
Muratam <Muratam@users.noreply.github.com>
2019-06-16 15:24:54 +0900

    Update kotlin.kt (195 bytes)

--- 05/kotlin.kt
+++ 05/kotlin.kt
@@ -1,6 +1,6 @@
-fun main(){var s = readLine()!!
+fun main(){var s=readLine()!!
 println(s)
-val r=Regex("T")
-val p=r.find(s)!!.range.start;for (m in generateSequence(::readLine)){val q=m.toCharArray()
-q[p]='r'
-println(Regex("K.*r").replace(String(q), {z->"r".repeat(z.value.length)}))}}
+while(0<1){var m=readLine()?:break
+var q=m.toCharArray()
+q[s.indexOf('T')]='r'
+println(Regex("K.*r").replace(String(q),{z->"r".repeat(z.value.length)}))}}
