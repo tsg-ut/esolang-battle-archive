10c7aa180b9e7bfc1a6c02a628dcb90674948270
cookie-s <cookie-s@users.noreply.github.com>
2019-06-16 16:01:17 +0900

    Update d-gdc.d (146 bytes)

--- 05/d-gdc.d
+++ 05/d-gdc.d
@@ -1,1 +1,1 @@
-import std.stdio;void main(){int b=0;char[] c,d,e;for(c=readln.dup;e=readln.dup,e;d=e)write(c);for(;d[++b]-75;){};for(;c[b]-84;)++c[b++];write(c);}
+import std.stdio;void main(){int b=0;char[] c,d,e;for(c=readln.dup;e=readln.dup,e;d=e)write(c);for(;d[++b]-75;){}for(;c[b]-84;)++c[b++];write(c);}
