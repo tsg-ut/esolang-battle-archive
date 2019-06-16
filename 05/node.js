57582fa4139f0bc976a809815729bd9f737099db
Muratam <Muratam@users.noreply.github.com>
2019-06-16 14:10:32 +0900

    Update node.js (149 bytes)

--- 05/node.js
+++ 05/node.js
@@ -1,2 +1,4 @@
 s=""+require("fs").readFileSync(0)
-console.log(s.replace(RegExp(`\n(.{${s.search(/T/)}}).`,"g"),"\n$1#").replace(/K *#/,m=>`K${"#".repeat(m.length-1)}`))
+console.log(s.replace(RegExp(`
+(.{${s.search(/T/)}}).`,"g"),`
+$1#`).replace(/K *#/,m=>"K"+"#".repeat(m.length-1)))
