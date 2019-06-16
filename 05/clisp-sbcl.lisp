8a065be5d3ab9b2571b26f9d6efa10656d678804
akouryy <akouryy@users.noreply.github.com>
2019-06-16 17:41:01 +0900

    Update clisp-sbcl.lisp (162 bytes)

--- 05/clisp-sbcl.lisp
+++ 05/clisp-sbcl.lisp
@@ -1,10 +1,1 @@
-(setq b nil)
-(setq e 1)
-
-(loop
-  (setq c (read-char))
-  (when (char= c #\T) (setq e 0))
-  (setq b (or (char= c #\K) (and b (> e 0))))
-  (princ (if (or (= e 0) b) #\a c))
-  (setq e (mod (+ e 1) 51))
-)
+(setq b nil)(setq e 1)(loop(setq c(read-char))(if(char= c #\T)(setq e 0))(setq b(or(char= c #\K)(and b(> e 0))))(princ(if(or(= e 0)b)#\a c))(setq e(mod(1+ e)51)))
