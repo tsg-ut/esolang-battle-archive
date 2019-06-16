12066635eebfa9c18aa1907ea47f4024accdfcba
akouryy <akouryy@users.noreply.github.com>
2019-06-16 13:49:41 +0900

    Update perl.pl (98 bytes)

--- 05/perl.pl
+++ 05/perl.pl
@@ -1,6 +1,1 @@
-$s=`dd`;
-$s=~/T/;
-$i=length$`;
-$s=~s/(?<=\n.{$i}) /#/g;
-$s=~s/(?<=K) *(?=#)/"#"x length$&/ge;
-print$s
+print$a=$b=<>;$a=~tr/T/*/;<>;$c=$_,print$a for<>;$_=$a^$b^$c;s/\^ *K|K *\^/'*'x length($&)/e;print
