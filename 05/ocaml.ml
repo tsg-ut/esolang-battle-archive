86badd379d158be94850d00efc177b71fb665e70
satos---jp <satos---jp@users.noreply.github.com>
2019-06-16 17:52:18 +0900

    Update ocaml.ml (159 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -1,3 +1,2 @@
 open String
-let rec f s t=try f(print_endline s;s)(read_line())with _->index s 'T'+1|>sub(sub t 0(index t 'K')^make 99 '*')0|>print_string
-let _=f(read_line())""
+let rec r s=let t=read_line()in s^"\n"^(try let p=index t 'K'in(make p ' ')^(make((index s 'T')-p+1)'*')with _->r s);;print_endline(r(read_line()))
