0a6f3cc65ebb097ab587618423679b6cc7a6acce
henkma <henkma@users.noreply.github.com>
2019-06-16 16:58:38 +0900

    Update haskell.hs (90 bytes)

--- 05/haskell.hs
+++ 05/haskell.hs
@@ -1,2 +1,3 @@
-main=interact$unlines.scanl1(!).lines
-a!b|(x,y)<-span(<'J')$zipWith max a b=x++scanr1 max y
+main=interact$unlines.s(!).lines
+a!b|(x,y)<-span(<'J')$zipWith max a b=x++s max y
+s=scanr1
