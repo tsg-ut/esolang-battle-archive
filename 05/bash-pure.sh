bfc5d9c9d1ce9abfe563a3108dcba66252dac9c0
hanazuki <hanazuki@users.noreply.github.com>
2019-06-17 04:12:59 +0900

    Update bash-pure.sh (100 bytes)

--- 05/bash-pure.sh
+++ 05/bash-pure.sh
@@ -1,1 +1,1 @@
-while read -N1 c;do c=`printf %d "'$c"`;x=$[c&4?0:-~x%51-c%2*51];printf \\$(printf %o $[c|=x<1]);done
+while read -N1 c;do c=`printf %d "'$c"`;x=$[c&4?0:-~x%51-c%2*51];printf \\`printf %o $[c|=x<1]`;done
