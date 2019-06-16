e37a2a485f9f214ad0704f0fa24608b8e178aa62
domperor <domperor@users.noreply.github.com>
2019-06-16 15:45:33 +0900

    Update bash-busybox.sh (189 bytes)

--- 05/bash-busybox.sh
+++ 05/bash-busybox.sh
@@ -1,9 +1,7 @@
 IFS= read -r a;while IFS= read -r b;do
-echo "$a"
-f="$b"
-g="$f"
+echo "$a";f="$b";g="$f"
 done
-c=`echo "$a"|sed -r 's/T.+$/T/'`
-d=`echo "$g"|sed -r 's/K.+$/K/'`
+c=`echo "$a"|sed -r 's/T.+/T/'`
+d=`echo "$g"|sed -r 's/K.+/K/'`
 printf "$d"
 printf "*%.s" `eval echo {1..$[${#c}-${#d}]}`
