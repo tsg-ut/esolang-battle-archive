a91dc372533c9cfa95530e00010d62384824aa63
dnek <dnek@users.noreply.github.com>
2019-06-16 23:45:46 +0900

    Update kotlin.kt (171 bytes)

--- 05/kotlin.kt
+++ 05/kotlin.kt
@@ -1,4 +1,4 @@
 fun main(){var s=readLine()!!
 println(s)
 val l=s.indexOf('T')
-while(0<1){println(Regex("K.*r").replace(readLine()!!.replaceRange(l,l,"r"),{z->"r".repeat(z.value.length)}))}}
+while(0<1)println(Regex("K.*r").replace(readLine()!!.replaceRange(l,l,"r"),{z->"r".repeat(z.value.length)}))}
