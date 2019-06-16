566ce6524bbbb725ba65220d9b47f506db89f24d
drafear <drafear@users.noreply.github.com>
2019-06-16 15:47:34 +0900

    Update ocaml.ml (162 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -1,3 +1,3 @@
 open String
-let rec f s t=try let t=read_line()in print_endline s;f s t with _->index s 'T'+1|>sub(sub t 0(index t 'K')^make 99 '*')0|>print_string
+let rec f s t=try f(print_endline s;s)(read_line()) with _->index s 'T'+1|>sub(sub t 0(index t 'K')^make 99 '*')0|>print_string
 let _=f(read_line())""
