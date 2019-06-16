e8221daf5d986c978a383eb2eff4703de9e20a5d
utgwkk <utgwkk@users.noreply.github.com>
2019-06-16 14:28:57 +0900

    Update ocaml.ml (213 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -2,5 +2,5 @@ open List
 open String
 let p=print_endline
 let l=ref 0
-let rec r()=try(let s=read_line()in
-if!l=0 then(l:=index s 'T';p s)else p(try(let i=index s 'K'in make i ' '^"K"^make(!l-i)'*')with _->make!l ' '^"*");r())with _->()let()=r()
+let rec r()=let s=read_line()in
+if!l=0 then p(l:=index s 'T';s)else p(try(let i=index s 'K'in make i ' '^"K"^make(!l-i)'*')with _->make!l ' '^"*");r()let()=r()
