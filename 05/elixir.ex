4a63c896f5bccc841b24c1a5e93b1b78b50b4a0e
nakario <nakario@users.noreply.github.com>
2019-06-16 15:14:47 +0900

    Update elixir.ex (336 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -1,16 +1,8 @@
 defmodule A do
-def d(s,n) do
-String.duplicate(s,n)
-end
-def e(s) do
-String.length(Enum.at(String.split(IO.read(:line),s),0))end
-def b(t,h) do
-c(t,e("K"),h)end
-def c(t,51,h)do
-b(t,h+1)end
-def c(t,k,h)do
-IO.puts d(" ",t)<>"T"<>d(" ",49-t)
-Enum.each(1..h,fn(_)->IO.puts d(" ",t)<>"*"<>d(" ",49-t)end)
-IO.puts d(" ",k)<>"K"<>d("*",t-k)<>d(" ",49-t)end
+def d(n,s\\" "),do: String.duplicate(s,n)
+def e(s),do: String.length(Enum.at(String.split(IO.read(:line),s),0))
+def b(t,h),do: c(t,e("K"),h)
+def c(t,51,h),do: b(t,h+1)
+def c(t,k,h),do: IO.puts Enum.join([d(t)<>"*"<>d(49-t)]++List.duplicate(d(t)<>"*"<>d(49-t),h)++[d(k)<>"*"<>d(t-k,"*")<>d(49-t)],"\n")
 end
 A.b(A.e("T"),0)
