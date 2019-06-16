9b5f6ae1d64fb88de1fa836d4923e62af25fc8b6
n4o847 <n4o847@users.noreply.github.com>
2019-06-16 19:03:51 +0900

    Update php.php (88 bytes)

--- 05/php.php
+++ 05/php.php
@@ -1,2 +1,2 @@
 <?php
-while($c=fgetc(STDIN)){$x++;echo$c!=T?$x%51-$t?$c!=K?$f!=1?$c:1:$f=1:($f=0)+1:($t=$x)*0+1;}
+while($c=fgetc(STDIN)){$x++;echo$c!=T?$x%51-$t?$c!=K?!$f?$c:1:$f=1:!$f=0:!!$t=$x;}
