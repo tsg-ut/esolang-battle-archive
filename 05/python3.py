d479da259af3fe4f770174b899153fd6002fa66f
pizzacat83 <pizzacat83@users.noreply.github.com>
2019-06-16 13:38:01 +0900

    Update python3.py (114 bytes)

--- 05/python3.py
+++ 05/python3.py
@@ -1,7 +1,5 @@
 i = input().find('T')
 while True:
-	print(' '*i+'*')
-	d = input().find('K')
-	if d>=0:
-		print(' '*d+'*'*(i-d+1))
-		break
+ print(' '*i+'*')
+ d = input().find('K')
+ if d>=0:print(' '*d+'*'*(i-d+1));break
