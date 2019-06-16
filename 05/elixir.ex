4278755ea8d05a79d42b2f6d038a07d5cfdf27d2
drafear <drafear@users.noreply.github.com>
2019-06-16 16:47:32 +0900

    Update elixir.ex (195 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -1,9 +1,6 @@
-defmodule F do
-def f(s,x,t)do
+f=fn s,x,t,f->
 case IO.gets x do
 :eof->IO.write String.slice String.trim_trailing(t)<>String.duplicate("*",99),0,String.length String.trim_trailing s
-t->f s,s,t
+t->f.(s,s,t,f)end
 end
-end
-end
-F.f IO.gets(""),"",""
+f.(IO.gets(""),"","",f)
