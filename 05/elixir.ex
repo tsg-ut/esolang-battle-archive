c5cd41bafa8995fef8fa32b6fa01ce4fef6c204b
drafear <drafear@users.noreply.github.com>
2019-06-16 16:37:53 +0900

    Update elixir.ex (216 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -3,8 +3,8 @@ def f(s,t)do
 case IO.gets("")do
 :eof->IO.write String.slice(String.trim_trailing(t)<>String.duplicate("*",99),0,String.length String.trim_trailing s)
 t->IO.write s
-f(s,t)
+f s,t
 end
 end
 end
-F.f(IO.gets(""),"")
+F.f IO.gets(""),""
