83e83f1fa05491b656c80048361e9c2c20f9b38b
utgwkk <utgwkk@users.noreply.github.com>
2019-06-16 14:57:10 +0900

    Update ocaml.ml (176 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -1,5 +1,4 @@
 open String
-let p=print_endline
 let l=ref 0
 let rec r()=let s=read_line()in
-p(let i=try index s 'K'with _->try l:=index s 'T';!l with _-> !l in make i ' '^make(!l-i+1)'*');r()let()=r()
+print_endline(let i=try index s 'K'with _->try l:=index s 'T';!l with _-> !l in make i ' '^make(!l-i+1)'*');r()let()=r()
