b486e217322905bf84a413f650124bfcee681896
sh-mug <sh-mug@users.noreply.github.com>
2019-06-16 13:59:06 +0900

    Update c-gcc.c (197 bytes)

--- 05/c-gcc.c
+++ 05/c-gcc.c
@@ -1,1 +1,1 @@
-s[90];main(p,i,q){gets(s);p=strchr(s,84)-(int)s;for(;gets(s),i=p;){for(;i--;)putchar(32);puts("*");q=strchr(s,75);if(q){for(i=q-(int)s;i--;)putchar(32);for(i=p-(q-(int)s)+1;i--;)putchar(42);break;}}}
+s[90];main(p,i,q,r){gets(s);p=strchr(s,84)-(int)s;for(;gets(s),i=p;){r=(int)s;for(;i--;)putchar(32);puts("*");q=strchr(s,75);if(q){for(i=q-r;i--;)putchar(32);for(r+=p-q+1;r--;)putchar(42);break;}}}
