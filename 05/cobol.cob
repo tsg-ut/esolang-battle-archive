9127eeb4fda8cea651e956391c26a3925e87ba81
hanazuki <hanazuki@users.noreply.github.com>
2019-06-17 06:02:26 +0900

    Update cobol.cob (377 bytes)

--- 05/cobol.cob
+++ 05/cobol.cob
@@ -1,2 +1,3 @@
        >>SOURCE FORMAT IS FREE
-id division.program-id.a.data division.working-storage section. 1 s pic x(50). 1 p pic x(50). 1 t pic 99. 1 k pic 99. procedure division.accept s.inspect s tallying t for leading " ".b.display s.accept p.move 0 to k.inspect p tallying k for all " ".if k = 50 then go to b end-if.string p(1 : t) "T" into s.inspect s replacing all " " by "*" after initial "K" before initial "T".display s.
+program-id.a.data division.working-storage section. 1 s pic x(50). 1 p pic x(50). 1 t pic 99. 1 k pic 99.
+procedure division.accept s.inspect s tallying t for leading" ".b.display s.accept p.move 0 to k.inspect p tallying k for all" ".if k=50 go b.string p(1:t)"T"into s.inspect s replacing all" "by"K"after initial"K"before initial"T".display s.
