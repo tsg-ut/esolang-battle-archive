43ed3fb42aa580b8eed12e2811b2f8f1475c8732
akouryy <akouryy@users.noreply.github.com>
2019-06-16 16:55:50 +0900

    Update swift.swift (163 bytes)

--- 05/swift.swift
+++ 05/swift.swift
@@ -3,10 +3,8 @@ while let l=readLine(){
 print(String(
 l.map{(c:Character) -> Character in
 if"T"==c{d=0}
-if"K"==c{b=true}else{b=b&&d>0}
-var e=c
-if d<1||b{e="a"}
+b="K"==c||b&&d>0
 d=(d+1)%50
-return e
+return d==1||b ?"a":c
 }))
 }
