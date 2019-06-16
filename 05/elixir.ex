dd4c28fb655f23dcc4c9f42c0102ae20c8235026
nakario <nakario@users.noreply.github.com>
2019-06-16 15:29:30 +0900

    Update elixir.ex (313 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -3,6 +3,6 @@ def d(n,s\\" "),do: String.duplicate(s,n)
 def e(s),do: String.length(Enum.at(String.split(IO.read(:line),s),0))
 def b(t,h),do: c(t,e("K"),h)
 def c(t,51,h),do: b(t,h+1)
-def c(t,k,h),do: IO.puts Enum.join([d(t)<>"*"<>d(49-t)]++List.duplicate(d(t)<>"*"<>d(49-t),h)++[d(k)<>"*"<>d(t-k,"*")<>d(49-t)],"\n")
+def c(t,k,h),do: IO.puts Enum.join(List.duplicate(d(t)<>"*"<>d(49-t),h+1)++[d(k)<>d(t-k+1,"*")<>d(49-t)],"\n")
 end
 A.b(A.e("T"),0)
