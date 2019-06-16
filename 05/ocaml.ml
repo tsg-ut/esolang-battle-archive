82d7964207906888bf8c45721122e1730329250d
utgwkk <utgwkk@users.noreply.github.com>
2019-06-16 18:02:38 +0900

    Update ocaml.ml (151 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -1,2 +1,2 @@
 open String
-let rec r s=let t=read_line()in s^"\n"^(try let p=index t 'K'in(make p ' ')^(make((index s 'T')-p+1)'*')with _->r s);;print_string(r(read_line()))
+let rec r s=let t=read_line()in s^"\n"^try let p=index t 'K'in make p ' '^make(index s 'T'-p+1)'*'with _->r s;;print_string(r(read_line()))
