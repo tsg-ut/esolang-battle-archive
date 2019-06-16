e0c821406ed32c9f8a9a47609e7fad6dac3a1491
n4o847 <n4o847@users.noreply.github.com>
2019-06-16 13:36:46 +0900

    Update ruby.rb (86 bytes)

--- 05/ruby.rb
+++ 05/ruby.rb
@@ -1,7 +1,4 @@
-t=1+gets.index(?T)
-s=1
-s+=1 while !u=gets.index(?K)
-k=" "*u
-puts k+?=*(t-u)+"
-"+(k+"=
-")*s
+s=`dd`
+s.gsub!(/^ {#{s[/^ *K/].size-1}}\K /,?#)
+s.gsub!(/# *(?=T)/){?#*$&.size}
+puts s
