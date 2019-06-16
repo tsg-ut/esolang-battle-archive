c97ac0bd7aed4a3fb3c772b239486a3afc81aff8
nakario <nakario@users.noreply.github.com>
2019-06-16 14:38:13 +0900

    Update elixir.ex (366 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -1,11 +1,16 @@
 defmodule A do
+def d(s,n) do
+String.duplicate(s,n)
+end
+def e(s) do
+String.length(Enum.at(String.split(IO.read(:line),s),0))end
 def b(t,h) do
-c(t,String.length(Enum.at(String.split(IO.read(:line),"K"),0)),h)end
+c(t,e("K"),h)end
 def c(t,51,h)do
 b(t,h+1)end
 def c(t,k,h)do
-IO.puts String.duplicate(" ",t)<>"T"<>String.duplicate(" ",49-t)
-Enum.each(1..h,fn(_)->IO.puts String.duplicate(" ",t)<>"*"<>String.duplicate(" ",49-t)end)
-IO.puts String.duplicate(" ",k)<>"K"<>String.duplicate("*",t-k)<>String.duplicate(" ",49-t)end
+IO.puts d(" ",t)<>"T"<>d(" ",49-t)
+Enum.each(1..h,fn(_)->IO.puts d(" ",t)<>"*"<>d(" ",49-t)end)
+IO.puts d(" ",k)<>"K"<>d("*",t-k)<>d(" ",49-t)end
 end
-A.b(String.length(Enum.at(String.split(IO.read(:line),"T"),0)),0)
+A.b(A.e("T"),0)
