5df5ea40ebda0b85fb900106a07ef448016e9c4c
cookie-s <cookie-s@users.noreply.github.com>
2019-06-16 16:07:33 +0900

    Update d-gdc.d (110 bytes)

--- 05/d-gdc.d
+++ 05/d-gdc.d
@@ -1,7 +1,1 @@
-import std.stdio;
-void main(){int b,c,d=1;
-for(;~(c=getchar());d=++d%51)
-  d*=c!=84,
-  b=c&1|b&&d,
-  putchar(!d|b?99:c);
-}
+import std.stdio;void main(){int b,c,d=1;for(;~(c=getchar());d=++d%51)d*=c!=84,b=c&1|b&&d,putchar(!d|b?99:c);}
