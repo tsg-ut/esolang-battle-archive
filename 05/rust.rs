dd28334b04f6ef1175b260e9ad416f34a5ddca32
kurgm <kurgm@users.noreply.github.com>
2019-06-16 21:53:00 +0900

    Update rust.rs (160 bytes)

--- 05/rust.rs
+++ 05/rust.rs
@@ -1,1 +1,1 @@
-use std::io::*;fn main(){let mut a:[u8;999]=[0;999];let m=stdin().read(&mut a).unwrap();let mut i=51;while a[i-1]!=117{a[i]=a[i]|a[i-51]|a[i-1]&1;i+=1;}stdout().write(&a[0..m]);}
+use std::io::*;fn main(){let mut a:[u8;999]=[0;999];stdin().read(&mut a);let mut i=51;while a[i-1]!=117{a[i]|=a[i-51]|a[i-1]&1;i+=1;}stdout().write(&a[0..i]);}
