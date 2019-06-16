563bb9fcda9fdf73373e88a610141cc534576640
akouryy <akouryy@users.noreply.github.com>
2019-06-16 18:26:30 +0900

    Update php.php (107 bytes)

--- 05/php.php
+++ 05/php.php
@@ -1,2 +1,2 @@
 <?php
-$b=0;$d=1;while($c=fread(STDIN,1)){if($c=='T')$d=0;$b=$c=='K'||$b&&$d;echo(!$d||$b?'A':$c);$d=($d+1)%51;}
+$b=0;$d=1;while($c=fread(STDIN,1)){$d*=$c!='T';$b=$c=='K'||$b&&$d;echo(!$d||$b?'A':$c);$d=($d+1)%51;}
