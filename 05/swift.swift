a7282ac2bef96939b79ead993c0534fe6d80ddf4
Muratam <Muratam@users.noreply.github.com>
2019-06-17 22:18:22 +0900

    Update swift.swift (113 bytes)

--- 05/swift.swift
+++ 05/swift.swift
@@ -1,3 +1,3 @@
 var x=0
-while let l=readLine(){l.map{x=$0=="T" ?0:$0=="K" ?x-49:(x+1)%50;print(x<1 ?"x":$0,terminator:"")}
+while let l=readLine(){l.map{x=$0>"S" ?0:$0>"J" ?x-49:(x+1)%50;print(x<1 ?"x":$0,terminator:"")}
 print()}
