00c6991ae7fb306dec3f8c47c1024029067fe80a
kurgm <kurgm@users.noreply.github.com>
2019-06-17 10:25:42 +0900

    Update lua.lua (102 bytes)

--- 05/lua.lua
+++ 05/lua.lua
@@ -1,2 +1,2 @@
 R=io.read"*a"
-print((R:gsub((" "):rep(50),R:sub(1,50)):gsub("K",("*"):rep(1+R:find("T")-R:find("K")%51))))
+print((R:gsub((" "):rep(50),R:sub(1,50)):gsub("K",("*"):rep(1+R:find"T"-R:find"K"%51))))
