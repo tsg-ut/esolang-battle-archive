d4e1d2aaa2502af333d0e8767c107e95139f6da5
JP3BGY <JP3BGY@users.noreply.github.com>
2019-06-17 18:02:13 +0900

    Update elixir.ex (166 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -1,5 +1,4 @@
 i=IO.read :all
-x="T"
-{t,_}=:binary.match i,x
+{t,_}=:binary.match i,"T"
 {k,_}=:binary.match i,"K"
-IO.puts for c<-0..byte_size(i)-1,do: (k<=c&&c<(k-(rem k,51)+t)||(rem c,51)==t)&&x||String.at(i,c)
+IO.puts for c<-0..byte_size(i)-1,do: (k<=c&&c<(k-(rem k,51)+t)||(rem c,51)==t)&&"T"||String.at(i,c)
