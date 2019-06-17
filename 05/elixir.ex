c9c6c8832ba3be0f4a9a911703e5dd53aa39ceea
hanazuki <hanazuki@users.noreply.github.com>
2019-06-18 04:54:07 +0900

    Update elixir.ex (137 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -1,12 +1,10 @@
 defmodule M do
 def f(x)do
-c=IO.read(1)
-x=case c do
-:eof->1/0
-"T"->0
-"K"->x-50
-_->rem(x+1,51)end
+""<>c=IO.read(1)
 IO.write(if x<1,do: "@",else: c)
-f(x)end
+f(case c do
+"T"->1
+"K"->x-50
+_->rem(x+1,51)end)end
 end
-M.f(0)
+M.f(1)
