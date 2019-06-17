c2630a916350004c9b553ba9d3245c302dfe85b2
n4o847 <n4o847@users.noreply.github.com>
2019-06-17 22:15:03 +0900

    Update swift.swift (115 bytes)

--- 05/swift.swift
+++ 05/swift.swift
@@ -1,3 +1,3 @@
 var x=0
-while let l=readLine(){for i in l{x=i=="T" ?0:i=="K" ?x-49:(x+1)%50;print(x<1 ?"x":i,terminator:"")}
+while let l=readLine(){l.map{x=$0=="T" ?0:$0=="K" ?x-49:(x+1)%50;print(x<1 ?"x":$0,terminator:"")}
 print()}
