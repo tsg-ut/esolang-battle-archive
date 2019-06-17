a92815332351f6e944d943a85393e7698ad00965
akouryy <akouryy@users.noreply.github.com>
2019-06-18 03:04:46 +0900

    Update java.java (170 bytes)

--- 05/java.java
+++ 05/java.java
@@ -1,15 +1,1 @@
-import java.util.Scanner;
-public class A{
-public static void main(String[]a){
-boolean b=false;
-int d=1;
-Scanner s=new Scanner(System.in);
-String g;
-while((g=s.nextLine())!=null){
-for(int i=0;i<g.length();++i){
-char c=g.charAt(i);
-if(c>83)d=0;
-b=c==75||b&&d>0;
-System.out.print(d==0||b?"A":c+"");
-d=++d%50;
-}System.out.println();}}}
+class A{public static void main(String[]a)throws Exception{int d=1,c;while((c=System.in.read())>0){if(c>83)d=0;d-=c%2*51;System.out.print(d<1?"A":(char)c+"");d=++d%51;}}}
