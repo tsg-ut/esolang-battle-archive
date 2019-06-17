dbadaed6d744843924a9d5e15df2df1ad5864bf6
akouryy <akouryy@users.noreply.github.com>
2019-06-18 03:08:48 +0900

    Update java.java (160 bytes)

--- 05/java.java
+++ 05/java.java
@@ -1,1 +1,1 @@
-class A{public static void main(String[]a)throws Exception{int d=1,c;while((c=System.in.read())>0){if(c>83)d=0;d-=c%2*51;System.out.print(d<1?"A":(char)c+"");d=++d%51;}}}
+class A{public static void main(String[]a)throws Exception{for(int d=1,c;(c=System.in.read())>0;d=c>83?1:(d-c%2*51+1)%51)System.out.print(d<1?"A":(char)c+"");}}
