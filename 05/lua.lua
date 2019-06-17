97f19110f891f04a9009e3fdb9d0b863050b931c
kurgm <kurgm@users.noreply.github.com>
2019-06-17 10:23:30 +0900

    Update lua.lua (106 bytes)

--- 05/lua.lua
+++ 05/lua.lua
@@ -1,6 +1,2 @@
-a=0
-i=1
-c={}while a<117 do
-s=io.read(1)i=i%51+1
-c[i]=(c[i]or 0)|s:byte(1)a=c[i]|a&1
-io.write(string.char(a))end
+R=io.read"*a"
+print((R:gsub((" "):rep(50),R:sub(1,50)):gsub("K",("*"):rep(1+R:find("T")-R:find("K")%51))))
