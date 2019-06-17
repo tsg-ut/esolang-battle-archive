ecb84c8bab8b5dee99902a6de9281c73a42220a4
hanazuki <hanazuki@users.noreply.github.com>
2019-06-17 18:43:52 +0900

    Update ocaml.ml (136 bytes)

--- 05/ocaml.ml
+++ 05/ocaml.ml
@@ -1,3 +1,3 @@
 let rec f x=let c=input_char stdin in
-let x=if c='T' then 51 else if c='K' then x-50 else (x+1)mod 51 in
+let x=match c with 'T'->51|'K'->x-50|_->(x+1)mod 51 in
 print_char(if x<1 then 'x' else c);f x;;f 0
