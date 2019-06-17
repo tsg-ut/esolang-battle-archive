d0201f2cd565810bcae1798f4a18d1b7662f92fa
dnek <dnek@users.noreply.github.com>
2019-06-17 22:31:15 +0900

    Update swift.swift (110 bytes)

--- 05/swift.swift
+++ 05/swift.swift
@@ -1,3 +1,4 @@
 var x=0
-while let l=readLine(){l.map{x=$0>"S" ?0:$0>"J" ?x-49:(x+1)%50;print(x<1 ?"x":$0,terminator:"")}
-print()}
+while let l=readLine(){var s=""
+l.map{x=$0>"S" ?0:$0>"J" ?x-49:(x+1)%50;s+="\(x<1 ?"x":$0)"}
+print(s)}
