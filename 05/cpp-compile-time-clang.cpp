7844988cc6887480a69910bb0d995d3ad66985a0
Muratam <Muratam@users.noreply.github.com>
2019-06-18 19:50:14 +0900

    Update cpp-compile-time-clang.cpp (110 bytes)

--- 05/cpp-compile-time-clang.cpp
+++ 05/cpp-compile-time-clang.cpp
@@ -1,2 +1,3 @@
 #define constexpr
-char o[3000],x,*p=o;char*f(const char*i){for(;*i;*p++=*i++|x<1)x=*i&4?0:-~x%51-*i%2*51;return o;}
+char o[3000],x,*p=o;
+#define f(i) ([](){for(;*i;*p++=*i++|x<1)x=*i&4?0:-~x%51-*i%2*51;}(),o)
