da86b9b0cb01ec9b053ce8e9530051333fa9258b
akouryy <akouryy@users.noreply.github.com>
2019-06-18 19:19:53 +0900

    Update java.java (147 bytes)

--- 05/java.java
+++ 05/java.java
@@ -1,1 +1,1 @@
-interface A{static void main(String[]a)throws Exception{for(int d=1,c;(c=System.in.read())>0;d=c>83?1:(d-c%2*51+1)%51)System.out.print(d<1?"A":(char)c+"");}}
+interface A{static void main(String[]a)throws Exception{for(int d=1,c;(c=System.in.read())>0;d=c>83?1:(d-c%2*51+1)%51)System.out.write(d<1?35:c);}}
