97cb5bdc8abe550f1fcb889984b4896e0524be06
drafear <drafear@users.noreply.github.com>
2019-06-19 01:42:25 +0900

    Update bash-pure.sh (97 bytes)

--- 05/bash-pure.sh
+++ 05/bash-pure.sh
@@ -1,1 +1,8 @@
-while read -N1 c;do c=`printf %d "'$c"`;x=$[c&4?0:-~x%51-c%2*51];printf \\`printf %o $[c|x<1]`;done
+IFS=
+f(){
+read -n1 x
+x=`printf %o \'$x`
+printf \\$[1/(~c[i]&5),i=i%51+1,x?a=c[i]|=x|a&1:12]
+f
+}
+f
