ca93ae824a484c2656cb0ff9669dca8f83d98b96
sh-mug <sh-mug@users.noreply.github.com>
2019-06-18 19:08:48 +0900

    Update verilog.v (162 bytes)

--- 05/verilog.v
+++ 05/verilog.v
@@ -1,3 +1,3 @@
-module c;reg[407:0]b={51{8'h20}}^42,c,i;initial begin
-c=$fgets(i,1<<31);while($fgets(c,1<<31))$write("%s",i);i^=b;while(!(i&(c^b)))i|=i<<8;$write("%s",i);end
+module c;reg[407:0]b=42,c,i,j={51{8'h20}};initial begin
+b^=j;c=$fgets(i,1<<31);while($fgets(c,1<<31))$write("%s",i);$write("%s",(c-i)&{51{8'h3F}}|j);end
 endmodule
