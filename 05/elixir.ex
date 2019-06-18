f2678df62e21abf9ef47170d60fb49e11011ebcc
drafear <drafear@users.noreply.github.com>
2019-06-18 15:23:06 +0900

    Update elixir.ex (121 bytes)

--- 05/elixir.ex
+++ 05/elixir.ex
@@ -1,5 +1,4 @@
 f=fn s,i,j,f->case IO.getn(s,1)do
-:eof->1
-t->f.(i<50&&i*j==0&&t||1,i<50&&"T"!=t&&i+1||0,t=="K"&&1||i*j,f)end
+t->j-1!=i&&f.(i<50&&j==0&&t||1,i<50&&"T"!=t&&i+1||0,t=="K"&&1||j,f)end
 end
 f.("",0,0,f)
