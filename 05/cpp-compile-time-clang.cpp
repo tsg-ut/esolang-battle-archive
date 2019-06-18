41afaf884604dee6f16bd332a927d12c9585da89
hanazuki <hanazuki@users.noreply.github.com>
2019-06-18 19:45:29 +0900

    Update cpp-compile-time-clang.cpp (115 bytes)

--- 05/cpp-compile-time-clang.cpp
+++ 05/cpp-compile-time-clang.cpp
@@ -1,2 +1,2 @@
 #define constexpr
-char o[3000],x,*p;char*f(const char*i){for(p=o;*i;*p++=*i++|x<1)x=*i&4?0:-~x%51-*i%2*51;return o;}
+char o[3000],x,*p=o;char*f(const char*i){for(;*i;*p++=*i++|x<1)x=*i&4?0:-~x%51-*i%2*51;return o;}
