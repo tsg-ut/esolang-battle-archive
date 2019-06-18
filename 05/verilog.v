98cc4f305f72bfa91ed4bcc686408d437a01738a
sh-mug <sh-mug@users.noreply.github.com>
2019-06-18 10:05:37 +0900

    Update verilog.v (280 bytes)

--- 05/verilog.v
+++ 05/verilog.v
@@ -1,28 +1,8 @@
-`define STDIN 32'h8000_0000
-
-module cat;
-	integer c, i=0, s=0, e=0, k=0;
-	initial begin
-		c = $fgetc(`STDIN);
-		while (c != -1) begin
-			if(c[4]) begin
-				s = i;
-			end else if(c[6]) begin
-				e = i;
-			end
-			i += 1;
-			c = $fgetc(`STDIN);
-		end
-		for(i=0; i<=e/51; i++) begin
-			for(c=0; c<51; c++) begin
-				if(c==s%51||(i==e/51&&c>=e%51&&c<=s%51)) begin
-					k = 8'h2a;
-				end else begin
-					k = 8'h20;
-				end
-				$write("%c", k);
-			end
-			$write("%c", 8'h0a);
-		end
-	end
+module c;integer c,i=0,s=0,e=0;initial begin
+c=$fgetc(1<<31);for(i=0;c+1;i++)begin if(c[4])s=i;if(c[6])e=i;c=$fgetc(1<<31);end
+for(i=0;i<=e/51;i++)begin
+for(c=0;c<51;c++)begin
+if(c==s%51||(i==e/51&&c>=e%51&&c<=s%51)) $write("*");else $write(" ");end
+$write("\n");end
+end
 endmodule
