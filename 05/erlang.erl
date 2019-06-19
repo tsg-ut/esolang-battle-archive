7d66beb1ae7f463bf1fa925c318624e19a9df5a7
hanazuki <hanazuki@users.noreply.github.com>
2019-06-20 04:29:17 +0900

    Update erlang.erl (129 bytes)

--- 05/erlang.erl
+++ 05/erlang.erl
@@ -1,3 +1,3 @@
 
 main(_)->f(1,"").
-f(X,S)->Y=case C=io:get_chars(S,1)of"T"->1;"K"->X-50;eof->halt();_->X+1 end rem 51,f(Y,if X<1->"@";0<1->C end).
+f(X,S)->Y=case C=io:get_chars(S,1)of"T"->1;"K"->X-50;eof->halt();_->X+1end rem 51,f(Y,if X<1->"@";0<1->C end).
