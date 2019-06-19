eaad3a804f7d7fd6e466445c506b7f04737e6d88
cookie-s <cookie-s@users.noreply.github.com>
2019-06-19 18:49:53 +0900

    Update erlang.erl (143 bytes)

--- 05/erlang.erl
+++ 05/erlang.erl
@@ -1,5 +1,5 @@
 %%!
 f(S)->case string:chr(io:get_line(S),$K)of
-0->f(S);J->io:format("~*c~*c",[J-1,$ ,1+string:chr(S,$T)-J,$*])
+0->f(S);J->io:format("~*cA~*c",[J-1,$ ,string:chr(S,$T)-J,$*])
 end.
 main(_)->f(io:get_line("")).
