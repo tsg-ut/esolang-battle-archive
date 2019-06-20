384fd3c8ec3515d1739bc6a1dec8408da417c3d9
kurgm <kurgm@users.noreply.github.com>
2019-06-20 19:07:17 +0900

    Update racket.rkt (143 bytes)

--- 05/racket.rkt
+++ 05/racket.rkt
@@ -1,6 +1,2 @@
-#lang racket
-(define (f s x y)
-(let ([t (regexp-replace x s y)]) (if (pair? (regexp-match x t)) (f t x y) t))
-)
-(display (f (f (car (regexp-match #px"\\D*" (current-input-port))) #px"([^T]{50}) " "\\1T") #px"K " "KK"))
-
+#!racket
+(define(main t l)(if(string>? l t)(main(string-replace t" T""TT")l)(begin(display t)(newline)(main t(read-line)))))(main(read-line)"")
