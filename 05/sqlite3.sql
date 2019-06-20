98899a5b4844498c14775551c6aa9b188962265f
utgwkk <utgwkk@users.noreply.github.com>
2019-06-20 16:44:55 +0900

    Update sqlite3.sql (133 bytes)

--- 05/sqlite3.sql
+++ 05/sqlite3.sql
@@ -1,1 +1,1 @@
-select substr(replace(v,substr(v,52,51),substr(v,0,52)),0,instr(v,'K'))||printf('%.'||(length(substr(v,instr(v,'K')))-50+instr(v,'T'))||'c','K')from i
+select substr(replace(v,substr(v,52,51),substr(v,0,52)),0,instr(v,'K'))||printf('K%.'||(instr(v,'T')-instr(v,'K')%51)||'c','K')from i
