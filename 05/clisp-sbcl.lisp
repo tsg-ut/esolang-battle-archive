54bfc9cc04aec38f06e7dc5ed95e56fcaceaed24
kurgm <kurgm@users.noreply.github.com>
2019-06-21 15:40:54 +0900

    Update clisp-sbcl.lisp (158 bytes)

--- 05/clisp-sbcl.lisp
+++ 05/clisp-sbcl.lisp
@@ -1,1 +1,1 @@
-(defun m(u k i)(let((c(read-char))(j(mod(+ i 1)51)))(princ(or k(if(= u i)"T"c)))(case c(#\T(m i k j))(#\K(m u c j))(t(or(and k(= u i))(m u k j))))))(m 0 nil 1)
+(defun m(u k i)(let((c(read-char))(j(mod(1+ i)51)))(princ(or k(if(= u i)"T"c)))(case c(#\T(m i k j))(#\K(m u c j))(t(or(and k(= u i))(m u k j))))))(m 0 nil 1)
